42642b7151b2bbc2917b81b69330c6fc4dde480e
Thorsten Hell <thorsten.hell@cismet.de>
2010-04-16 14:32:53 +0000

    [issue:0001450] neue Methoden des Bindable INterfaces implementiert

--- src/main/java/de/cismet/cids/custom/objecteditors/commons/JaNeinNullComboForBoolean.java
+++ src/main/java/de/cismet/cids/custom/objecteditors/commons/JaNeinNullComboForBoolean.java
@@ -1,48 +1,69 @@
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * To change this template, choose Tools | Templates and open the template in the editor.
  */
 package de.cismet.cids.custom.objecteditors.commons;
 
 import de.cismet.cids.editors.Bindable;
+
 import org.jdesktop.beansbinding.Converter;
 import org.jdesktop.beansbinding.Validator;
 
+
 /**
+ * DOCUMENT ME!
  *
- * @author thorsten
+ * @author   thorsten
+ * @version  $Revision$, $Date$
  */
 public class JaNeinNullComboForBoolean extends JaNeinNullCombo implements Bindable {
-
-    public String getBindingProperty() {
-        return "selectedItem";
-    }
     transient Converter<Boolean, String> c = new Converter<Boolean, String>() {
 
-        @Override
-        public String convertForward(Boolean b) {
-            if (b == null || b == false) {
-                return "Nein";
-            } else {
-                return "Ja";
-            }
-        }
+            @Override public String convertForward(Boolean b) {
 
-        @Override
-        public Boolean convertReverse(String s) {
-            if (s != null && s.equals("Ja")) {
-                return true;
-            } else {
-                return false;
+                if ((b == null) || (b == false)) {
+                    return "Nein";
+                }
+                else {
+                    return "Ja";
+                }
             }
-        }
-    };
 
-    public Converter getConverter() {
+            @Override public Boolean convertReverse(String s) {
+
+                if ((s != null) && s.equals("Ja")) {
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+        };
+
+    @Override public String getBindingProperty() {
+        return "selectedItem";
+    }
+
+    @Override public Converter getConverter() {
         return c;
     }
 
-    public Validator getValidator() {
+    @Override public Validator getValidator() {
         return null;
     }
+
+    @Override public Object getErrorSourceValue() {
+        return Boolean.FALSE;
+    }
+
+    @Override public Object getNullSourceValue() {
+        return Boolean.FALSE;
+    }
+
 }
