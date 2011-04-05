3e84d9da4098f3357bb60d3cc11abbf21a37a8e2
Thorsten Hell <thorsten.hell@cismet.de>
2011-04-05 08:16:32 +0000

    [issue:2006] AlkisUtils: Parameter nmless (Meaningless Number) zu den URLs hinzugefügt :-)

--- src/main/java/de/cismet/cids/custom/objectrenderer/utils/alkis/AlkisUtil.java
+++ src/main/java/de/cismet/cids/custom/objectrenderer/utils/alkis/AlkisUtil.java
@@ -71,8 +71,11 @@ public final class AlkisUtil {
             SRS_SERVICE = serviceProperties.getProperty("SRS_SERVICE");
             MAP_CALL_STRING = serviceProperties.getProperty("MAP_CALL_STRING") + SRS_GEOM;
             GEO_BUFFER = Double.parseDouble(serviceProperties.getProperty("GEO_BUFFER"));
+
             //
             PRODUCTS = new Products(USER, PASSWORD, SERVICE);
+
+            
         } catch (Exception ex) {
 //            log.error("AlkisCommons Error!", ex);
             throw new RuntimeException(ex);
@@ -84,6 +87,7 @@ public final class AlkisUtil {
     public final String PASSWORD;
     public final String SERVICE;
     public final String SERVER;
+    public final static String MLESSNUMBER="nmless=5061756C612030352E31322E32303035204A75737475732032352E30372E323030382054616E6A612030362E31302E31393734";;
     public final String CATALOG_SERVICE;
     public final String INFO_SERVICE;
     public final String SEARCH_SERVICE;
@@ -648,7 +652,8 @@ public final class AlkisUtil {
          * @param  format      DOCUMENT ME!
          */
         public void productEinzelNachweis(final String objectID, final String productCode) {
-            final String url = COMMONS.EINZEL_NACHWEIS_SERVICE +  "?product=" + productCode
+            final String url = COMMONS.EINZEL_NACHWEIS_SERVICE +  "?"+ AlkisUtil.MLESSNUMBER
+                    + "&product=" + productCode
                     + "&id=" + objectID+"&"+IDENTIFICATION;
             log.info("Open product URL : " + url);
             ObjectRendererUtils.openURL(url);
@@ -672,7 +677,8 @@ public final class AlkisUtil {
          * @param  format      DOCUMENT ME!
          */
         public void productListenNachweis(final String punktliste, final String productCode) {
-            final String url = COMMONS.LISTEN_NACHWEIS_SERVICE + "?product=" + productCode
+            final String url = COMMONS.LISTEN_NACHWEIS_SERVICE +  "?"+ AlkisUtil.MLESSNUMBER 
+                    + "&product=" + productCode
                     + "&ids=" + punktliste+"&"+IDENTIFICATION;
             log.info("Open product URL : " + url);
             ObjectRendererUtils.openURL(url);
@@ -706,7 +712,8 @@ public final class AlkisUtil {
          * @param  parcelCode  DOCUMENT ME!
          */
         public void productKarte(final String parcelCode) {
-            final String url = COMMONS.LIEGENSCHAFTSKARTE_SERVICE + "?landparcel=" + parcelCode+"&"+IDENTIFICATION;
+            final String url = COMMONS.LIEGENSCHAFTSKARTE_SERVICE +  "?"+ AlkisUtil.MLESSNUMBER 
+                    + "&landparcel=" + parcelCode+"&"+IDENTIFICATION;
             log.info("Open product URL : " + url);
             ObjectRendererUtils.openURL(url);
         }
@@ -730,7 +737,8 @@ public final class AlkisUtil {
                 final String zusText,
                 final String auftragsNr,
                 final boolean moreThanOneParcel) {
-            String url = COMMONS.LIEGENSCHAFTSKARTE_SERVICE + "?landparcel=" + parcelCode
+            String url = COMMONS.LIEGENSCHAFTSKARTE_SERVICE +  "?"+ AlkisUtil.MLESSNUMBER
+                    + "&landparcel=" + parcelCode
                     + "&angle=" + winkel
                     + "&product=" + produkt.getCode()
                     + "&centerx=" + centerX + "&centery=" + centerY;
