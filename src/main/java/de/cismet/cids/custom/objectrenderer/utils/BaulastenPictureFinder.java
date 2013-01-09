2ccf5b55f00ce6b8faf17b66fe1887ee090185af
helllth <th@cismet.de>
2013-01-09 14:11:06 +0100

    working on #34 switched from File to URL and switched from stored paths to automatically generated paths

--- src/main/java/de/cismet/cids/custom/objectrenderer/utils/BaulastenPictureFinder.java
+++ src/main/java/de/cismet/cids/custom/objectrenderer/utils/BaulastenPictureFinder.java
@@ -9,13 +9,17 @@ package de.cismet.cids.custom.objectrenderer.utils;
 
 import java.io.File;
 
+import java.net.HttpURLConnection;
 import java.net.URL;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import de.cismet.cids.custom.wunda_blau.res.StaticProperties;
 
 import de.cismet.tools.collections.TypeSafeCollections;
+
+import de.cismet.tools.gui.log4jquickconfig.Log4JQuickConfig;
 
 /**
  * DOCUMENT ME!
@@ -27,40 +31,136 @@ public final class BaulastenPictureFinder {
 
     //~ Static fields/initializers ---------------------------------------------
 
+    public static final String SEP = "/";
     static final org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(BaulastenPictureFinder.class);
     private static final String[] SUFFIXE = new String[] { "tif", "jpg", "tiff", "jpeg" };
 //    "TIF", "JPG", "TIFF", "JPEG"};
+    public static final String PATH = "http://s102is/Baulasten/";
 
     //~ Methods ----------------------------------------------------------------
 
     /**
      * DOCUMENT ME!
      *
-     * @param   picture  DOCUMENT ME!
+     * @param   blattnummer     picture DOCUMENT ME!
+     * @param   laufendeNummer  DOCUMENT ME!
      *
      * @return  DOCUMENT ME!
      */
-    public static List<File> findPlanPicture(final String picture) {
-        final String picturePath = StaticProperties.ALB_PLAN_URL_PREFIX + picture + ".";
+    public static List<URL> findPlanPicture(final String blattnummer, final String laufendeNummer) {
+        final String picturePath = getPlanPictureFilename(blattnummer, laufendeNummer);
         if (log.isDebugEnabled()) {
             log.debug("findPlanPicture: " + picturePath);
         }
-        return probeForRightSuffix(picturePath);
+        return probeWebserverForRightSuffix(picturePath);
     }
 
     /**
      * DOCUMENT ME!
      *
-     * @param   picture  DOCUMENT ME!
+     * @param   blattnummer     picture DOCUMENT ME!
+     * @param   laufendeNummer  DOCUMENT ME!
      *
      * @return  DOCUMENT ME!
      */
-    public static List<File> findTextblattPicture(final String picture) {
-        final String picturePath = StaticProperties.ALB_TEXTBLATT_URL_PREFIX + picture + ".";
+    public static List<URL> findTextblattPicture(final String blattnummer, final String laufendeNummer) {
+        final String picturePath = getTextblattPictureFilename(blattnummer, laufendeNummer);
         if (log.isDebugEnabled()) {
             log.debug("findTextblattPicture: " + picturePath);
         }
-        return probeForRightSuffix(picturePath);
+        return probeWebserverForRightSuffix(picturePath);
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @param   blattnummer     DOCUMENT ME!
+     * @param   laufendeNummer  DOCUMENT ME!
+     *
+     * @return  DOCUMENT ME!
+     */
+    public static String getTextblattPictureFilename(final String blattnummer, final String laufendeNummer) {
+        final String ret = getObjectFilename(blattnummer, laufendeNummer);
+
+        return (ret != null) ? new StringBuffer(ret).append("b.").toString() : null;
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @param   blattnummer     DOCUMENT ME!
+     * @param   laufendeNummer  DOCUMENT ME!
+     *
+     * @return  DOCUMENT ME!
+     */
+    private static String getObjectFilename(final String blattnummer, final String laufendeNummer) {
+        if (laufendeNummer == null) {
+            return null;
+        } else {
+            final int lfdNr = new Integer(laufendeNummer);
+            String trenner = "-";
+            int number = 0;
+            if (blattnummer.length() == 6) {
+                number = new Integer(blattnummer);
+            } else {
+                // length==7
+                number = new Integer(blattnummer.substring(0, 6));
+                trenner = blattnummer.substring(6, 7);
+            }
+
+            return new StringBuffer(getFolder(number)).append(SEP)
+                        .append(String.format("%06d", number))
+                        .append(trenner)
+                        .append(String.format("%02d", lfdNr))
+                        .toString();
+        }
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @param   blattnummer     DOCUMENT ME!
+     * @param   laufendeNummer  DOCUMENT ME!
+     *
+     * @return  DOCUMENT ME!
+     */
+    public static String getPlanPictureFilename(final String blattnummer, final String laufendeNummer) {
+        final String ret = getObjectFilename(blattnummer, laufendeNummer);
+
+        return (ret != null) ? new StringBuffer(ret).append("p.").toString() : null;
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @param   number  DOCUMENT ME!
+     *
+     * @return  DOCUMENT ME!
+     */
+    private static String getFolder(final int number) {
+        int lowerBorder = number - (number % 1000);
+        final int higherBorder = lowerBorder + 1000;
+        if (lowerBorder != 0) {
+            lowerBorder += 1;
+        }
+
+        final String lb = String.format("%06d", lowerBorder);
+        final String hb = String.format("%06d", higherBorder);
+        return new StringBuffer(PATH).append(lb).append("-").append(hb).toString();
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @param  args  DOCUMENT ME!
+     */
+    public static void main(final String[] args) {
+        Log4JQuickConfig.configure4LumbermillOnLocalhost();
+        System.out.println(getFolder(50));
+        System.out.println(getFolder(1050));
+        System.out.println(getFolder(1000));
+        System.out.println(getFolder(1001));
+        System.out.println(getFolder(12001));
     }
 
     /**
@@ -70,7 +170,7 @@ public final class BaulastenPictureFinder {
      *
      * @return  DOCUMENT ME!
      */
-    private static List<File> probeForRightSuffix(final String fileWithoutSuffix) {
+    private static List<File> probeFilesystemForRightSuffix(final String fileWithoutSuffix) {
         if (log.isDebugEnabled()) {
             log.debug("Searching for picture: " + fileWithoutSuffix + "xxx");
         }
@@ -78,12 +178,47 @@ public final class BaulastenPictureFinder {
         for (final String suffix : SUFFIXE) {
             try {
                 final URL fileURL = new URL(fileWithoutSuffix + suffix);
+
                 final File testFile = new File(fileURL.toURI());
                 if (testFile.isFile()) {
                     if (log.isDebugEnabled()) {
                         log.debug("Found picture in file: " + testFile.getAbsolutePath());
                     }
                     results.add(testFile);
+                }
+            } catch (Exception ex) {
+                log.error(ex, ex);
+            }
+        }
+        if (log.isDebugEnabled()) {
+            log.debug("No picture file found.");
+        }
+        return results;
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @param   fileWithoutSuffix  DOCUMENT ME!
+     *
+     * @return  DOCUMENT ME!
+     */
+    private static List<URL> probeWebserverForRightSuffix(final String fileWithoutSuffix) {
+        if (log.isDebugEnabled()) {
+            log.debug("Searching for picture: " + fileWithoutSuffix + "xxx");
+        }
+        final List<URL> results = new ArrayList<URL>();
+
+        for (final String suffix : SUFFIXE) {
+            try {
+                final URL objectURL = new URL(fileWithoutSuffix + suffix);
+
+                final HttpURLConnection huc = (HttpURLConnection)objectURL.openConnection();
+                huc.setRequestMethod("GET");
+                huc.connect();
+                final int reponse = huc.getResponseCode();
+                if (reponse == 200) {
+                    results.add(objectURL);
                 }
             } catch (Exception ex) {
                 log.error(ex, ex);
