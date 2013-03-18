95e54d914f6f5b4e9705983cd6a0f1812d9ae002
Gilles Baatz <gilles.baatz@cismet.de>
2013-03-18 16:39:56 +0100

    #59 changed filename of generated pdfs of Flurstücke to product.parcelCodeIncreasingNumber.pdf

--- src/main/java/de/cismet/cids/custom/objectrenderer/wunda_blau/AlkisLandparcelRenderer.java
+++ src/main/java/de/cismet/cids/custom/objectrenderer/wunda_blau/AlkisLandparcelRenderer.java
@@ -685,12 +685,13 @@ public class AlkisLandparcelRenderer extends javax.swing.JPanel implements Borde
                         return;
                     }
 
+                    final String filename = product + "." + parcelCode.replaceAll("/", "--");
                     final HttpDownload download = new HttpDownload(
                             url,
                             "",
                             DownloadManagerDialog.getJobname(),
                             downloadTitle,
-                            product,
+                            filename,
                             extension);
                     DownloadManager.instance().add(download);
                 }
