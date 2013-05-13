02946f6a9fe19177cf5935e751d3c4cbb95750f9
Daniel Meiers <daniel.meiers@cismet.de>
2013-05-13 13:04:14 +0200

    #66 visualising state in the download title, polling server results with delay, making NASDownload cancellable

--- src/main/java/de/cismet/cids/custom/nas/NASDownload.java
+++ src/main/java/de/cismet/cids/custom/nas/NASDownload.java
@@ -16,9 +16,21 @@ import Sirius.navigator.exception.ConnectionException;
 
 import com.vividsolutions.jts.geom.Geometry;
 
+import org.openide.util.Cancellable;
+import org.openide.util.Exceptions;
+import org.openide.util.NbBundle;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 import de.cismet.cids.custom.wunda_blau.search.actions.NasDataQueryAction;
 
@@ -32,13 +44,34 @@ import de.cismet.tools.gui.downloadmanager.AbstractDownload;
  * @author   daniel
  * @version  $Revision$, $Date$
  */
-public class NASDownload extends AbstractDownload {
+public class NASDownload extends AbstractDownload implements Cancellable {
 
     //~ Static fields/initializers ---------------------------------------------
 
     private static String SEVER_ACTION = "nasDataQuery";
     private static String EXTENSION = ".xml";
     private static String STANDARD_FILE_NAME = "nas-result";
+    private static final String BASE_TITLE;
+
+    static {
+        BASE_TITLE = NbBundle.getMessage(
+                NASDownload.class,
+                "NASDownload.basetitle.text");
+    }
+
+    //~ Enums ------------------------------------------------------------------
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @version  $Revision$, $Date$
+     */
+    private enum Phase {
+
+        //~ Enum constants -----------------------------------------------------
+
+        REQEUST_GEN, RETRIEVAL, DOWNLOAD, DONE
+    }
 
     //~ Instance fields --------------------------------------------------------
 
@@ -61,7 +94,7 @@ public class NASDownload extends AbstractDownload {
         this.orderId = orderId;
         template = null;
         geometry = null;
-        this.title = "NAS Download";
+        this.title = BASE_TITLE;
         status = State.WAITING;
         this.directory = "";
         filename = orderId;
@@ -113,6 +146,7 @@ public class NASDownload extends AbstractDownload {
         this.filename = filename;
 //        determineDestinationFile(filename, EXTENSION);
     }
+
     /**
      * Creates a new NASDownload object.
      */
@@ -123,14 +157,29 @@ public class NASDownload extends AbstractDownload {
     //~ Methods ----------------------------------------------------------------
 
     @Override
+    public boolean cancel() {
+        downloadFuture.cancel(true);
+        status = State.ABORTED;
+        stateChanged();
+        return downloadFuture.isCancelled();
+    }
+
+    @Override
     public void run() {
         if (status != State.WAITING) {
             return;
         }
-
+        /*
+         * Phase 1: sending the reqeust to the server
+         */
+        setTitleForPhase(Phase.REQEUST_GEN);
         status = State.RUNNING;
         stateChanged();
         if (!omitSendingRequest) {
+            if (Thread.interrupted()) {
+                log.warn("NAS Download was interuppted");
+                return;
+            }
             orderId = sendNasRequest();
             if (orderId == null) {
                 log.error("nas server request returned no orderId, cannot continue with NAS download");
@@ -142,13 +191,33 @@ public class NASDownload extends AbstractDownload {
                 filename = orderId;
             }
         }
-
-        status = State.WAITING;
+        setTitleForPhase(Phase.RETRIEVAL);
         stateChanged();
 
-        content = getNasResult();
+        /*
+         * Phase 2: retrive the result from the cids server
+         */
+        if (Thread.interrupted()) {
+            log.warn("NAS Download was interuppted");
+            cancelNasRequest();
+            return;
+        }
+        final ExecutorService executor = Executors.newSingleThreadExecutor();
+        final Future<ByteArrayWrapper> pollingFuture = executor.submit(new ServerPollingRunnable());
+        try {
+            content = pollingFuture.get(1, TimeUnit.HOURS).getByteArray();
+        } catch (InterruptedException ex) {
+            log.warn("NAS Download was interuppted", ex);
+            cancelNasRequest();
+            return;
+        } catch (ExecutionException ex) {
+            log.warn("could not execute nas download", ex);
+            Exceptions.printStackTrace(ex);
+        } catch (TimeoutException ex) {
+            log.warn("the maximum timeout for nas download is exceeded", ex);
+        }
 
-        status = State.RUNNING;
+        setTitleForPhase(Phase.DOWNLOAD);
         stateChanged();
 
         if ((content == null) || (content.length <= 0)) {
@@ -158,6 +227,14 @@ public class NASDownload extends AbstractDownload {
                 status = State.COMPLETED_WITH_ERROR;
                 stateChanged();
             }
+            return;
+        }
+        /*
+         * Phase 3: save the result file
+         */
+        if (Thread.interrupted()) {
+            log.warn("NAS Download was interuppted");
+            cancelNasRequest();
             return;
         }
 
@@ -184,10 +261,26 @@ public class NASDownload extends AbstractDownload {
             }
         }
 
-        if (status == State.RUNNING) {
-            status = State.COMPLETED;
-            stateChanged();
+        setTitleForPhase(Phase.DONE);
+        status = State.COMPLETED;
+        stateChanged();
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @param  p  DOCUMENT ME!
+     */
+    private void setTitleForPhase(final Phase p) {
+        String appendix = "";
+        if (p == Phase.REQEUST_GEN) {
+            appendix = NbBundle.getMessage(NASDownload.class, "NASDownload.requestGenTitle.text");
+        } else if (p == Phase.RETRIEVAL) {
+            appendix = NbBundle.getMessage(NASDownload.class, "NASDownload.resultRetrievalTitle.text");
+        } else if (p == Phase.DOWNLOAD) {
+            appendix = NbBundle.getMessage(NASDownload.class, "NASDownload.downloadTitle.text");
         }
+        title = BASE_TITLE + " - " + appendix;
     }
 
     /**
@@ -222,31 +315,112 @@ public class NASDownload extends AbstractDownload {
 
     /**
      * DOCUMENT ME!
-     *
-     * @return  DOCUMENT ME!
      */
-    private byte[] getNasResult() {
-        final ServerActionParameter paramMethod = new ServerActionParameter(NasDataQueryAction.PARAMETER_TYPE.METHOD
-                        .toString(),
-                NasDataQueryAction.METHOD_TYPE.GET);
-
+    private void cancelNasRequest() {
         final ServerActionParameter paramOrderId = new ServerActionParameter(NasDataQueryAction.PARAMETER_TYPE.ORDER_ID
                         .toString(),
                 orderId);
-        byte[] result = null;
+        final ServerActionParameter paramMethod = new ServerActionParameter(NasDataQueryAction.PARAMETER_TYPE.METHOD
+                        .toString(),
+                NasDataQueryAction.METHOD_TYPE.CANCEL);
         try {
-            while (result == null) {
-                result = (byte[])SessionManager.getProxy()
-                            .executeTask(
-                                    SEVER_ACTION,
-                                    "WUNDA_BLAU",
-                                    null,
-                                    paramOrderId,
-                                    paramMethod);
-            }
+            SessionManager.getProxy().executeTask(
+                SEVER_ACTION,
+                "WUNDA_BLAU",
+                null,
+                paramOrderId,
+                paramMethod);
         } catch (Exception ex) {
-            log.error("error during pulling nas result from server", ex);
+            log.error("error during enqueuing nas server request", ex);
         }
-        return result;
+    }
+
+    //~ Inner Classes ----------------------------------------------------------
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @version  $Revision$, $Date$
+     */
+    private class ServerPollingRunnable implements Callable<ByteArrayWrapper> {
+
+        //~ Methods ------------------------------------------------------------
+
+        @Override
+        public ByteArrayWrapper call() {
+            final ServerActionParameter paramMethod = new ServerActionParameter(NasDataQueryAction.PARAMETER_TYPE.METHOD
+                            .toString(),
+                    NasDataQueryAction.METHOD_TYPE.GET);
+
+            final ServerActionParameter paramOrderId = new ServerActionParameter(
+                    NasDataQueryAction.PARAMETER_TYPE.ORDER_ID.toString(),
+                    orderId);
+            byte[] result = null;
+            while (result == null) {
+                try {
+                    result = (byte[])SessionManager.getProxy()
+                                .executeTask(
+                                        SEVER_ACTION,
+                                        "WUNDA_BLAU",
+                                        null,
+                                        paramOrderId,
+                                        paramMethod);
+                } catch (ConnectionException ex) {
+                    log.error("error during pulling nas result from server", ex);
+                }
+                if (result == null) {
+                    try {
+                        Thread.sleep(5000);
+                    } catch (InterruptedException ex) {
+                        log.error("result fetching thread was interrupted", ex);
+                    }
+                }
+            }
+            final ByteArrayWrapper baw = new ByteArrayWrapper(result);
+            return baw;
+        }
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @version  $Revision$, $Date$
+     */
+    private class ByteArrayWrapper {
+
+        //~ Instance fields ----------------------------------------------------
+
+        private byte[] byteArray;
+
+        //~ Constructors -------------------------------------------------------
+
+        /**
+         * Creates a new ByteArrayWrapper object.
+         *
+         * @param  ba  DOCUMENT ME!
+         */
+        private ByteArrayWrapper(final byte[] ba) {
+            byteArray = ba;
+        }
+
+        //~ Methods ------------------------------------------------------------
+
+        /**
+         * DOCUMENT ME!
+         *
+         * @return  DOCUMENT ME!
+         */
+        public byte[] getByteArray() {
+            return byteArray;
+        }
+
+        /**
+         * DOCUMENT ME!
+         *
+         * @param  b  DOCUMENT ME!
+         */
+        public void setByteArray(final byte[] b) {
+            this.byteArray = b;
+        }
     }
 }
