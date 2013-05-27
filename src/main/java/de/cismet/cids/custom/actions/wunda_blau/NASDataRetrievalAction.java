47a89e28b43b35aaa41a04f9ca860432fe42889d
Daniel Meiers <daniel.meiers@cismet.de>
2013-05-27 15:53:58 +0200

    #66 using the new NasGui in the nas context action

--- src/main/java/de/cismet/cids/custom/actions/wunda_blau/NASDataRetrievalAction.java
+++ src/main/java/de/cismet/cids/custom/actions/wunda_blau/NASDataRetrievalAction.java
@@ -11,27 +11,29 @@
  */
 package de.cismet.cids.custom.actions.wunda_blau;
 
+import Sirius.navigator.connection.SessionManager;
+import Sirius.navigator.exception.ConnectionException;
+
 import com.vividsolutions.jts.geom.Geometry;
 
+import org.openide.util.Exceptions;
 import org.openide.util.lookup.ServiceProvider;
 
 import java.awt.event.ActionEvent;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 import javax.swing.AbstractAction;
-import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
 
-import de.cismet.cids.custom.nas.NASDownload;
-import de.cismet.cids.custom.wunda_blau.search.actions.NasDataQueryAction;
+import de.cismet.cids.custom.nas.NasDialog;
+import de.cismet.cids.custom.wunda_blau.search.actions.NasZaehlObjekteSearch;
 
-import de.cismet.cismap.commons.CrsTransformer;
 import de.cismet.cismap.commons.features.CommonFeatureAction;
 import de.cismet.cismap.commons.features.Feature;
-import de.cismet.cismap.commons.interaction.CismapBroker;
 
 import de.cismet.tools.gui.StaticSwingTools;
-import de.cismet.tools.gui.downloadmanager.DownloadManager;
-import de.cismet.tools.gui.downloadmanager.DownloadManagerDialog;
 
 /**
  * DOCUMENT ME!
@@ -87,35 +89,13 @@ public class NASDataRetrievalAction extends AbstractAction implements CommonFeat
 
     @Override
     public void actionPerformed(final ActionEvent e) {
-        // show mask to ask for the Product Template
-        final Object[] possibilities = { "Komplett", "Ohne Eigentuemer", "Nur Punkte" };
-        final String s = (String)JOptionPane.showInputDialog(
-                StaticSwingTools.getParentFrame(CismapBroker.getInstance().getMappingComponent()),
-                "Please choose a template",
-                "Product Template",
-                JOptionPane.PLAIN_MESSAGE,
-                null,
-                possibilities,
-                "Komplett");
-        final Geometry g = f.getGeometry();
-        CrsTransformer.transformToGivenCrs(g, DEFAULT_CRS);
-        NasDataQueryAction.PRODUCT_TEMPLATE template;
-        if (s.equals("Komplett")) {
-            template = NasDataQueryAction.PRODUCT_TEMPLATE.KOMPLETT;
-        } else if (s.equals("Ohne Eigentuemer")) {
-            template = NasDataQueryAction.PRODUCT_TEMPLATE.OHNE_EIGENTUEMER;
-        } else {
-            template = NasDataQueryAction.PRODUCT_TEMPLATE.POINTS;
-        }
-        if (DownloadManagerDialog.showAskingForUserTitle(
-                        CismapBroker.getInstance().getMappingComponent())) {
-            final String jobname = (!DownloadManagerDialog.getJobname().equals("")) ? DownloadManagerDialog
-                            .getJobname() : null;
-            DownloadManager.instance().add(
-                new NASDownload("NAS-Download", jobname, "", template, f.getGeometry()));
-        } else {
-            DownloadManager.instance().add(
-                new NASDownload("NAS-Download", "", template, f.getGeometry()));
-        }
+        final NasDialog dialog = new NasDialog(null, false);
+        SwingUtilities.invokeLater(new Runnable() {
+
+                @Override
+                public void run() {
+                    StaticSwingTools.showDialog(dialog);
+                }
+            });
     }
 }
