504c540342927c192e7150ac495a22d3c2d6ac2e
Matthias Roncoroni <matthias.roncoroni@cismet.de>
2013-07-01 11:16:47 +0200

    #76 corrected the height of all instances of SearchControlPanel to fit the height of the GeoSearchButton

--- src/main/java/de/cismet/cids/custom/wunda_blau/search/MauernWindowSearch.java
+++ src/main/java/de/cismet/cids/custom/wunda_blau/search/MauernWindowSearch.java
@@ -200,13 +200,13 @@ public class MauernWindowSearch extends javax.swing.JPanel implements CidsWindow
         final Dimension pre = pnlSearchCancel.getPreferredSize();
         pnlSearchCancel.setMaximumSize(new java.awt.Dimension(
                 new Double(max.getWidth()).intValue(),
-                new Double(max.getHeight() + 6).intValue()));
+                new Double(max.getHeight() + 5).intValue()));
         pnlSearchCancel.setMinimumSize(new java.awt.Dimension(
                 new Double(min.getWidth()).intValue(),
-                new Double(min.getHeight() + 6).intValue()));
+                new Double(min.getHeight() + 5).intValue()));
         pnlSearchCancel.setPreferredSize(new java.awt.Dimension(
                 new Double(pre.getWidth() + 6).intValue(),
-                new Double(pre.getHeight() + 6).intValue()));
+                new Double(pre.getHeight() + 5).intValue()));
         pnlButtons.add(pnlSearchCancel);
 
         metaClass = ClassCacheMultiple.getMetaClass(CidsBeanSupport.DOMAIN_NAME, "mauer");
