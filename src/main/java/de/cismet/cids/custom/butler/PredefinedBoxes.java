9fb8facf30bc7155e9c4f74a9a0ce518636a43c2
Daniel Meiers <daniel.meiers@cismet.de>
2013-07-03 14:30:11 +0200

    #73 adding all needed predefined box sizes and add them sorted in the combobox

--- src/main/java/de/cismet/cids/custom/butler/PredefinedBoxes.java
+++ src/main/java/de/cismet/cids/custom/butler/PredefinedBoxes.java
@@ -16,6 +16,8 @@ import org.apache.log4j.Logger;
 import java.io.IOException;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Enumeration;
 import java.util.Properties;
 
@@ -29,7 +31,7 @@ public class PredefinedBoxes {
 
     //~ Static fields/initializers ---------------------------------------------
 
-    protected static ArrayList<PredefinedBoxes> elements = new ArrayList<PredefinedBoxes>();
+    protected static final ArrayList<PredefinedBoxes> elements = new ArrayList<PredefinedBoxes>();
     private static final Logger LOG = Logger.getLogger(PredefinedBoxes.class);
 
     static {
@@ -37,8 +39,22 @@ public class PredefinedBoxes {
         try {
             prop.load(PredefinedBoxes.class.getResourceAsStream("predefinedBoxes.properties"));
             final Enumeration keys = prop.propertyNames();
+            final ArrayList<String> keyList = new ArrayList<String>();
             while (keys.hasMoreElements()) {
                 final String key = (String)keys.nextElement();
+                keyList.add(key);
+            }
+            final Comparator<String> keyComp = new Comparator<String>() {
+
+                    @Override
+                    public int compare(final String o1, final String o2) {
+                        final int number1 = Integer.parseInt(o1.replaceAll("box", ""));
+                        final int number2 = Integer.parseInt(o2.replaceAll("box", ""));
+                        return Integer.compare(number1, number2);
+                    }
+                };
+            Collections.sort(keyList, keyComp);
+            for (final String key : keyList) {
                 final String[] splittedVal = ((String)prop.getProperty(key)).split(";");
                 elements.add(new PredefinedBoxes(
                         splittedVal[0],
@@ -46,7 +62,7 @@ public class PredefinedBoxes {
                         Double.parseDouble(splittedVal[2])));
             }
         } catch (IOException ex) {
-            LOG.error("Could not read property file with defined boxes for butler 1");
+            LOG.error("Could not read property file with defined boxes for butler 1",ex);
         }
     }
 
