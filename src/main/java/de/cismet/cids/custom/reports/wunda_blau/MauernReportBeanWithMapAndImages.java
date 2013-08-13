616c7915c76792b2f9f51b7f039fd5e53f795045
Daniel Meiers <daniel.meiers@cismet.de>
2013-08-13 15:40:27 +0200

    fixes #97

--- src/main/java/de/cismet/cids/custom/reports/wunda_blau/MauernReportBeanWithMapAndImages.java
+++ src/main/java/de/cismet/cids/custom/reports/wunda_blau/MauernReportBeanWithMapAndImages.java
@@ -13,21 +13,19 @@ package de.cismet.cids.custom.reports.wunda_blau;
 
 import com.vividsolutions.jts.geom.Geometry;
 
-import org.openide.util.Exceptions;
 import org.openide.util.NbBundle;
 
 import java.awt.Color;
-import java.awt.Graphics2D;
 import java.awt.Image;
-import java.awt.image.BufferedImage;
 
 import java.io.InputStream;
 
 import java.text.NumberFormat;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 import javax.imageio.ImageIO;
 
@@ -35,16 +33,12 @@ import de.cismet.cids.custom.objectrenderer.utils.CidsBeanSupport;
 
 import de.cismet.cids.dynamics.CidsBean;
 
-import de.cismet.cismap.commons.BoundingBox;
-import de.cismet.cismap.commons.Crs;
+import de.cismet.cismap.commons.HeadlessMapProvider;
 import de.cismet.cismap.commons.XBoundingBox;
 import de.cismet.cismap.commons.features.DefaultStyledFeature;
-import de.cismet.cismap.commons.gui.MappingComponent;
-import de.cismet.cismap.commons.gui.layerwidget.ActiveLayerModel;
+import de.cismet.cismap.commons.interaction.CismapBroker;
 import de.cismet.cismap.commons.raster.wms.simple.SimpleWMS;
 import de.cismet.cismap.commons.raster.wms.simple.SimpleWmsGetMapUrl;
-import de.cismet.cismap.commons.retrieval.RetrievalEvent;
-import de.cismet.cismap.commons.retrieval.RetrievalListener;
 
 import de.cismet.netutil.Proxy;
 
@@ -102,7 +96,6 @@ public class MauernReportBeanWithMapAndImages extends MauernReportBean {
         WEB_DAV_USER = bundle.getString("user");
         WEB_DAV_DIRECTORY = bundle.getString("url");
         this.webDavClient = new WebDavClient(Proxy.fromPreferences(), WEB_DAV_USER, WEB_DAV_PASSWORD, true);
-        final ArrayList al = new ArrayList();
 //        final SimpleWMS s = new SimpleWMS(new SimpleWmsGetMapUrl("http://www.wms.nrw.de/geobasis/DOP?&VERSION=1.1.1&REQUEST=GetMap&WIDTH=<cismap:width>&HEIGHT=<cismap:height>&BBOX=<cismap:boundingBox>&SRS=EPSG:25832&FORMAT=image/png&TRANSPARENT=TRUE&BGCOLOR=0xF0F0F0&EXCEPTIONS=application/vnd.ogc.se_xml&LAYERS=WMS,0,Metadaten&STYLES="));
 //        final SimpleWMS s = new SimpleWMS(new SimpleWmsGetMapUrl(
 //                    "http://s102x284:8399/arcgis/services/WuNDa-Orthophoto-WUP/MapServer/WMSServer?service=WMS&VERSION=1.1.1&REQUEST=GetMap&WIDTH=<cismap:width>&HEIGHT=<cismap:height>&BBOX=<cismap:boundingBox>&SRS=EPSG:25832&FORMAT=image/png&TRANSPARENT=TRUE&BGCOLOR=0xF0F0F0&EXCEPTIONS=application/vnd.ogc.se_xml&LAYERS=6&STYLES=default"));
@@ -111,116 +104,59 @@ public class MauernReportBeanWithMapAndImages extends MauernReportBean {
 
 //        final SimpleWMS s = new SimpleWMS(new SimpleWmsGetMapUrl(
 //                    "http://S102X284:8399/arcgis/services/ALKIS-EXPRESS/MapServer/WMSServer?&VERSION=1.1.1&REQUEST=GetMap&FORMAT=image/png&TRANSPARENT=FALSE&BGCOLOR=0xF0F0F0&EXCEPTIONS=application/vnd.ogc.se_xml&LAYERS=2,4,5,6,7,8,10,11,12,13,14,16,17,18,19,20,21,22,23,25,26,27,28,29,30,31,32,33,34,35,36,37,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100&STYLES=&BBOX=<cismap:boundingBox>&WIDTH=<cismap:width>&HEIGHT=<cismap:height>&SRS="));
-        final MappingComponent map = new MappingComponent(false);
-
-        final ActiveLayerModel mappingModel = new ActiveLayerModel();
-        s.addRetrievalListener(new RetrievalListener() {
-
-                @Override
-                public void retrievalStarted(final RetrievalEvent e) {
-                    System.out.println("Start");
-                }
-
-                @Override
-                public void retrievalProgress(final RetrievalEvent e) {
-                }
-
-                @Override
-                public void retrievalComplete(final RetrievalEvent e) {
-                    new Thread() {
-
-                        @Override
-                        public void run() {
-                            try {
-                                try {
-                                    Thread.sleep(500);
-                                } catch (InterruptedException ex) {
-                                }
-                                final java.awt.Image img = map.getCamera()
-                                            .toImage(map.getWidth(), map.getHeight(), new Color(255, 255, 255, 0));
-                                final BufferedImage bi = new BufferedImage(
-                                        img.getWidth(null),
-                                        img.getHeight(null),
-                                        BufferedImage.TYPE_INT_RGB);
-                                final Graphics2D g2 = bi.createGraphics();
-                                // Draw img into bi so we can write it to file.
-                                g2.drawImage(img, 0, 0, null);
-                                g2.dispose();
-                                final BoundingBox bb = map.getCurrentBoundingBox();
-                                final String geolink = "http://localhost:" + 9098 + "/gotoBoundingBox?x1=" + bb.getX1()
-                                            + "&y1=" + bb.getY1() + "&x2=" + bb.getX2() + "&y2=" + bb.getY2();     // NOI18N
-                                mapImage = bi;
-                                System.out.println("karte geladen " + bi);
-                            } catch (Exception ex) {
-                                Exceptions.printStackTrace(ex);
-                            }
-                        }
-                    }.start();
-                }
-
-                @Override
-                public void retrievalAborted(final RetrievalEvent e) {
-                    mapError = true;
-                }
-
-                @Override
-                public void retrievalError(final RetrievalEvent e) {
-                    mapError = true;
-                }
-            });
-        // disable internal layer widget
-        map.setInternalLayerWidgetAvailable(false);
-        mappingModel.setSrs(new Crs("EPSG:25832", "", "", true, true));
-        mappingModel.addHome(new XBoundingBox(
-                374271.251964098,
-                5681514.032498134,
-                374682.9413952776,
-                5681773.852810634,
-                "EPSG:25832",
-                true));
-        // set the model
-        map.setMappingModel(mappingModel);
-        final int height = Integer.parseInt(NbBundle.getMessage(
-                    MauernReportBeanWithMapAndImages.class,
-                    "MauernReportBeanWithMapAndImages.mapHeight"));
-        final int width = Integer.parseInt(NbBundle.getMessage(
-                    MauernReportBeanWithMapAndImages.class,
-                    "MauernReportBeanWithMapAndImages.mapWidth"));
-        final int factor = MAP_DPI / 72;
-        map.setSize(width * factor, height * factor);
-//        map.setSize(width, height);
-//        map.setSize(540, 219);
-        // initial positioning of the map
-        map.setAnimationDuration(0);
-        map.gotoInitialBoundingBox();
-        map.unlock();
         final Geometry g = (Geometry)mauer.getProperty("georeferenz.geo_field");
 
         final DefaultStyledFeature dsf = new DefaultStyledFeature();
         dsf.setGeometry(g);
         dsf.setLineWidth(5);
         dsf.setLinePaint(Color.RED);
-        map.getFeatureCollection().addFeature(dsf);
 
-        map.zoomToFeatureCollection();
-        double scale;
-        if (map.getScaleDenominator() <= 750) {
-            scale = 750;
+        final HeadlessMapProvider mapProvider = new HeadlessMapProvider();
+        mapProvider.addLayer(s);
+        mapProvider.addFeature(dsf);
+        mapProvider.setMinimumScaleDenomimator(750);
+        mapProvider.setRoundScaleTo(HeadlessMapProvider.RoundingPrecision.HUNDRETH);
+        mapProvider.setCenterMapOnResize(true);
+
+        final int height = Integer.parseInt(NbBundle.getMessage(
+                    MauernReportBeanWithMapAndImages.class,
+                    "MauernReportBeanWithMapAndImages.mapHeight"));
+        final int width = Integer.parseInt(NbBundle.getMessage(
+                    MauernReportBeanWithMapAndImages.class,
+                    "MauernReportBeanWithMapAndImages.mapWidth"));
+        final XBoundingBox boundingBox;
+        if (CismapBroker.getInstance().getMappingComponent() != null) {
+            boundingBox = new XBoundingBox(g);
+            boundingBox.setX1(boundingBox.getX1());
+            boundingBox.setY1(boundingBox.getY1());
+            boundingBox.setX2(boundingBox.getX2());
+            boundingBox.setY2(boundingBox.getY2());
         } else {
-            scale = Math.round((map.getScaleDenominator() / 100) + 0.5d) * 100;
+            // only for test purposes
+            boundingBox = new XBoundingBox(
+                    33298653.1,
+                    5994912.610934,
+                    33308958.598,
+                    5999709.97916,
+                    "EPSG:35833",
+                    true);
         }
-        masstab = "1:" + NumberFormat.getIntegerInstance().format(scale);
-        if (LOG.isDebugEnabled()) {
-            LOG.debug("Scale for katasterblatt: " + masstab);
+//        mapProvider.setBoundingBox(xbb);
+        mapProvider.setBoundingBox(boundingBox);
+        final Future<Image> f = mapProvider.getImage(72, MAP_DPI, width, height);
+        try {
+            final Image img = f.get();
+            masstab = "1:" + NumberFormat.getIntegerInstance().format(mapProvider.getImageScaleDenominator());
+            mapImage = img;
+        } catch (InterruptedException ex) {
+            mapError = true;
+        } catch (ExecutionException ex) {
+            mapError = true;
         }
-        map.gotoBoundingBoxWithHistory(map.getBoundingBoxFromScale(scale));
-        map.setInteractionMode(MappingComponent.SELECT);
 
-        mappingModel.addLayer(s);
-
-        // TODO set correct url
         final List<CidsBean> images = CidsBeanSupport.getBeanCollectionFromProperty(mauer, "bilder");
         final StringBuilder url0Builder = new StringBuilder();
+
         final StringBuilder url1Builder = new StringBuilder();
         for (final CidsBean b : images) {
             final Integer nr = (Integer)b.getProperty("laufende_nummer");
