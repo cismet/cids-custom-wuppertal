5f83370475d6c15b7ef48a746603caf96af5e076
Daniel Meiers <daniel.meiers@cismet.de>
2013-08-21 16:12:23 +0200

    #101 showing error text as long as no product is selected

--- src/main/java/de/cismet/cids/custom/butler/Butler1ProductPanel.java
+++ src/main/java/de/cismet/cids/custom/butler/Butler1ProductPanel.java
@@ -320,6 +320,7 @@ public class Butler1ProductPanel extends javax.swing.JPanel implements ListSelec
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 0;
         gridBagConstraints.gridy = 0;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.WEST;
         gridBagConstraints.insets = new java.awt.Insets(10, 10, 20, 10);
         pnlFeeParrameter.add(lblVolumeParamTitle, gridBagConstraints);
 
