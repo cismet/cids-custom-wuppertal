f1e69a560fc86ddefe077abd7fbf08aca5897b9e
Gilles Baatz <gilles.baatz@cismet.de>
2014-02-24 15:33:22 +0100

    #147 BillingStatisticsReport uses JasperReportDownload. The class does not extends AbstractJasperReportPrint anymore.

--- src/main/java/de/cismet/cids/custom/reports/wunda_blau/BillingStatisticsReport.java
+++ src/main/java/de/cismet/cids/custom/reports/wunda_blau/BillingStatisticsReport.java
@@ -13,38 +13,30 @@ package de.cismet.cids.custom.reports.wunda_blau;
 
 import Sirius.navigator.ui.ComponentRegistry;
 
+import net.sf.jasperreports.engine.JRDataSource;
 import net.sf.jasperreports.engine.JasperPrint;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.ExecutionException;
 
-import javax.swing.SwingUtilities;
 import javax.swing.SwingWorker;
-
-import de.cismet.cids.custom.objectrenderer.utils.AbstractJasperReportPrint;
 
 import de.cismet.cids.dynamics.CidsBean;
 
-import de.cismet.cids.utils.jasperreports.ReportSwingWorkerDialog;
+import de.cismet.cismap.commons.gui.printing.JasperReportDownload;
 
-import de.cismet.cismap.commons.gui.printing.JasperDownload;
-import de.cismet.cismap.commons.interaction.CismapBroker;
-
-import de.cismet.tools.gui.StaticSwingTools;
 import de.cismet.tools.gui.downloadmanager.DownloadManager;
 import de.cismet.tools.gui.downloadmanager.DownloadManagerDialog;
 
 /**
- * DOCUMENT ME!
+ * Gets the needed information about the billing statistics report and generates a download for that report.
  *
  * @author   Gilles Baatz
  * @version  $Revision$, $Date$
  */
-public class BillingStatisticsReport extends AbstractJasperReportPrint {
+public class BillingStatisticsReport {
 
     //~ Static fields/initializers ---------------------------------------------
 
@@ -73,15 +65,6 @@ public class BillingStatisticsReport extends AbstractJasperReportPrint {
     private int amountWiederverkaeufeGB = 0;
 
     //~ Constructors -----------------------------------------------------------
-
-    /**
-     * Creates a new BillingStatisticsReport object.
-     *
-     * @param  bean  DOCUMENT ME!
-     */
-    public BillingStatisticsReport(final CidsBean bean) {
-        super(REPORT_URL, bean);
-    }
 
     /**
      * Creates a new BillingStatisticsReport object.
@@ -123,9 +106,6 @@ public class BillingStatisticsReport extends AbstractJasperReportPrint {
             final double earningsWithCostsWiederver,
             final int amountWiederverkaeufe,
             final int amountWiederverkaeufeGB) {
-        super(REPORT_URL, (CidsBean)beans.toArray()[0]);
-        setBeansCollection(true);
-
         this.from = from;
         this.till = till;
         this.amountTotalDownloads = amountTotalDownloads;
@@ -147,109 +127,68 @@ public class BillingStatisticsReport extends AbstractJasperReportPrint {
 
     //~ Methods ----------------------------------------------------------------
 
-    @Override
-    public Map generateReportParam(final CidsBean current) {
-        return generateReportParam(new ArrayList<CidsBean>());
-    }
-
-    @Override
-    public Map generateReportParam(final Collection<CidsBean> beans) {
-        final HashMap params = new HashMap();
-
-        params.put("from", from);
-        if (till == null) {
-            params.put("till", from);
-        } else {
-            params.put("till", till);
-        }
-
-        params.put("amountTotalDownloads", amountTotalDownloads);
-        params.put("amountWithCosts", amountWithCosts);
-        params.put("amountWithoutCosts", amountWithoutCosts);
-        params.put("amountWithCostsVU", amountWithCostsVU);
-        params.put("amountWithCostsWiederver", amountWithCostsWiederver);
-
-        params.put("earningsWithCostsVU", earningsWithCostsVU);
-        params.put("earningsWithCostsWiederver", earningsWithCostsWiederver);
-
-        params.put("amountVUamtlicherLageplan", amountVUamtlicherLageplan);
-        params.put("amountVUamtlicherLageplanGB", amountVUamtlicherLageplanGB);
-        params.put("amountVUhoheitlicheVermessung", amountVUhoheitlicheVermessung);
-        params.put("amountVUhoheitlicheVermessungGB", amountVUhoheitlicheVermessungGB);
-        params.put("amountVUsonstige", amountVUsonstige);
-        params.put("amountVUsonstigeGB", amountVUsonstigeGB);
-        params.put("amountWiederverkaeufe", amountWiederverkaeufe);
-        params.put("amountWiederverkaeufeGB", amountWiederverkaeufeGB);
-
-        return params;
-    }
-    @Override
-    public void print() {
-        final SwingWorker<JasperPrint, Void> old = downloadWorker;
-        if ((old != null) && !old.isDone()) {
-            old.cancel(true);
-        }
-        downloadWorker = new DownloadWorker();
-        downloadWorker.execute();
-    }
-
-    //~ Inner Classes ----------------------------------------------------------
-
     /**
      * DOCUMENT ME!
-     *
-     * @version  $Revision$, $Date$
      */
-    class DownloadWorker extends SwingWorker<JasperPrint, Void> {
+    public void generateReport() {
+        final JasperReportDownload.JasperReportDataSourceGenerator dataSourceGenerator =
+            new JasperReportDownload.JasperReportDataSourceGenerator() {
 
-        //~ Instance fields ----------------------------------------------------
+                @Override
+                public JRDataSource generateDataSource() {
+                    return null;
+                }
+            };
 
-        ReportSwingWorkerDialog dialog = new ReportSwingWorkerDialog(StaticSwingTools.getParentFrame(
-                    CismapBroker.getInstance().getMappingComponent()),
-                true);
+        final JasperReportDownload.JasperReportParametersGenerator parametersGenerator =
+            new JasperReportDownload.JasperReportParametersGenerator() {
 
-        //~ Methods ------------------------------------------------------------
+                @Override
+                public Map generateParamters() {
+                    final HashMap params = new HashMap();
 
-        @Override
-        protected JasperPrint doInBackground() throws Exception {
-            SwingUtilities.invokeLater(new Runnable() {
-
-                    @Override
-                    public void run() {
-                        StaticSwingTools.showDialog(dialog);
+                    params.put("from", from);
+                    if (till == null) {
+                        params.put("till", from);
+                    } else {
+                        params.put("till", till);
                     }
-                });
 
-            if (isCancelled()) {
-                return null;
-            } else {
-                return getJasperPrint();
-            }
-        }
+                    params.put("amountTotalDownloads", amountTotalDownloads);
+                    params.put("amountWithCosts", amountWithCosts);
+                    params.put("amountWithoutCosts", amountWithoutCosts);
+                    params.put("amountWithCostsVU", amountWithCostsVU);
+                    params.put("amountWithCostsWiederver", amountWithCostsWiederver);
 
-        @Override
-        protected void done() {
-            try {
-                final JasperPrint jasperPrint = get();
+                    params.put("earningsWithCostsVU", earningsWithCostsVU);
+                    params.put("earningsWithCostsWiederver", earningsWithCostsWiederver);
 
-                if (DownloadManagerDialog.showAskingForUserTitle(ComponentRegistry.getRegistry().getMainWindow())) {
-                    final String jobname = DownloadManagerDialog.getJobname();
-                    final String filename = "buchungen_geschaeftsstatistik";
-                    final String title = "Buchungen: Geschäftsstatistik";
+                    params.put("amountVUamtlicherLageplan", amountVUamtlicherLageplan);
+                    params.put("amountVUamtlicherLageplanGB", amountVUamtlicherLageplanGB);
+                    params.put("amountVUhoheitlicheVermessung", amountVUhoheitlicheVermessung);
+                    params.put("amountVUhoheitlicheVermessungGB", amountVUhoheitlicheVermessungGB);
+                    params.put("amountVUsonstige", amountVUsonstige);
+                    params.put("amountVUsonstigeGB", amountVUsonstigeGB);
+                    params.put("amountWiederverkaeufe", amountWiederverkaeufe);
+                    params.put("amountWiederverkaeufeGB", amountWiederverkaeufeGB);
 
-                    DownloadManager.instance().add(new JasperDownload(
-                            jasperPrint,
+                    return params;
+                }
+            };
+
+        if (DownloadManagerDialog.showAskingForUserTitle(ComponentRegistry.getRegistry().getMainWindow())) {
+            final String jobname = DownloadManagerDialog.getJobname();
+            final String filename = "buchungen_geschaeftsstatistik";
+            final String title = "Buchungen: Geschäftsstatistik";
+
+            DownloadManager.instance()
+                    .add(new JasperReportDownload(
+                            REPORT_URL,
+                            parametersGenerator,
+                            dataSourceGenerator,
                             jobname,
                             title,
                             filename));
-                }
-            } catch (InterruptedException ex) {
-                log.warn(ex, ex);
-            } catch (ExecutionException ex) {
-                log.error(ex, ex);
-            } finally {
-                dialog.setVisible(false);
-            }
         }
     }
 }
