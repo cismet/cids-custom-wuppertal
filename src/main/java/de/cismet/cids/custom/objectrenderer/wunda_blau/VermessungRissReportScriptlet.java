6bad018e67e3dd23f7ef2644e7a455078b9890ac
Gilles Baatz <gilles.baatz@cismet.de>
2014-03-17 14:10:44 +0100

    #157 in VermessungRissReportScriptlet.isImageAvailable() use WebAccessManager.checkIfURLaccessible() instead of writting that logic itself. Furthermore a HEAD request is used to check if the URL is available.

--- src/main/java/de/cismet/cids/custom/objectrenderer/wunda_blau/VermessungRissReportScriptlet.java
+++ src/main/java/de/cismet/cids/custom/objectrenderer/wunda_blau/VermessungRissReportScriptlet.java
@@ -11,13 +11,8 @@ import net.sf.jasperreports.engine.JRDefaultScriptlet;
 
 import org.apache.log4j.Logger;
 
-import org.openide.util.Exceptions;
-
 import java.awt.Color;
 import java.awt.image.BufferedImage;
-
-import java.io.IOException;
-import java.io.InputStream;
 
 import java.net.URL;
 
@@ -25,13 +20,7 @@ import java.util.Map;
 
 import de.cismet.cids.custom.objecteditors.wunda_blau.VermessungRissEditor;
 
-import de.cismet.security.AccessHandler;
 import de.cismet.security.WebAccessManager;
-
-import de.cismet.security.exceptions.AccessMethodIsNotSupportedException;
-import de.cismet.security.exceptions.MissingArgumentException;
-import de.cismet.security.exceptions.NoHandlerForURLException;
-import de.cismet.security.exceptions.RequestFailedException;
 
 import de.cismet.tools.gui.Static2DTools;
 
@@ -72,98 +61,13 @@ public class VermessungRissReportScriptlet extends JRDefaultScriptlet {
                 schluessel,
                 blatt);
 
-        InputStream streamToReadFrom = null;
+        boolean imageAvailable = false;
         for (final Map.Entry<URL, URL> urls : validURLs.entrySet()) {
             final URL url = urls.getKey();
-
-            try {
-                streamToReadFrom = WebAccessManager.getInstance().doRequest(url);
+            if (WebAccessManager.getInstance().checkIfURLaccessible(url)) {
+                imageAvailable = true;
                 break;
-            } catch (final MissingArgumentException ex) {
-                LOG.warn("Could not read document from URL '" + url.toExternalForm() + "'. Skipping this url.", ex);
-            } catch (final AccessMethodIsNotSupportedException ex) {
-                LOG.warn("Can't access document URL '" + url.toExternalForm()
-                            + "' with default access method. Skipping this url.",
-                    ex);
-            } catch (final RequestFailedException ex) {
-                LOG.warn("Requesting document from URL '" + url.toExternalForm() + "' failed. Skipping this url.",
-                    ex);
-            } catch (final NoHandlerForURLException ex) {
-                LOG.warn("Can't handle URL '" + url.toExternalForm() + "'. Skipping this url.", ex);
-            } catch (final Exception ex) {
-                LOG.warn("An exception occurred while opening URL '" + url.toExternalForm()
-                            + "'. Skipping this url.",
-                    ex);
             }
-        }
-
-        final boolean imageAvailable = streamToReadFrom != null;
-        try {
-            if (streamToReadFrom != null) {
-                streamToReadFrom.close();
-            }
-        } catch (IOException ex) {
-            LOG.warn(ex.getMessage(), ex);
-        }
-        return imageAvailable;
-    }
-
-    /**
-     * DOCUMENT ME!
-     *
-     * @param   host        DOCUMENT ME!
-     * @param   schluessel  DOCUMENT ME!
-     * @param   gemarkung   DOCUMENT ME!
-     * @param   flur        DOCUMENT ME!
-     * @param   blatt       DOCUMENT ME!
-     *
-     * @return  DOCUMENT ME!
-     */
-    public static Boolean isImageAvailable_head(final String host,
-            final String schluessel,
-            final Integer gemarkung,
-            final String flur,
-            final String blatt) {
-        final Map<URL, URL> validURLs = VermessungRissEditor.getCorrespondingURLs(
-                host,
-                gemarkung,
-                flur,
-                schluessel,
-                blatt);
-
-        InputStream streamToReadFrom = null;
-        for (final Map.Entry<URL, URL> urls : validURLs.entrySet()) {
-            final URL url = urls.getKey();
-
-            try {
-                streamToReadFrom = WebAccessManager.getInstance()
-                            .doRequest(url, "", AccessHandler.ACCESS_METHODS.HEAD_REQUEST);
-                break;
-            } catch (final MissingArgumentException ex) {
-                LOG.warn("Could not read document from URL '" + url.toExternalForm() + "'. Skipping this url.", ex);
-            } catch (final AccessMethodIsNotSupportedException ex) {
-                LOG.warn("Can't access document URL '" + url.toExternalForm()
-                            + "' with default access method. Skipping this url.",
-                    ex);
-            } catch (final RequestFailedException ex) {
-                LOG.warn("Requesting document from URL '" + url.toExternalForm() + "' failed. Skipping this url.",
-                    ex);
-            } catch (final NoHandlerForURLException ex) {
-                LOG.warn("Can't handle URL '" + url.toExternalForm() + "'. Skipping this url.", ex);
-            } catch (final Exception ex) {
-                LOG.warn("An exception occurred while opening URL '" + url.toExternalForm()
-                            + "'. Skipping this url.",
-                    ex);
-            }
-        }
-
-        final boolean imageAvailable = streamToReadFrom != null;
-        try {
-            if (streamToReadFrom != null) {
-                streamToReadFrom.close();
-            }
-        } catch (IOException ex) {
-            LOG.warn(ex.getMessage(), ex);
         }
         return imageAvailable;
     }
