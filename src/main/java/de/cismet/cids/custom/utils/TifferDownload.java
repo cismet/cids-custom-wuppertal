cae9b649d2785cae540d7ddbd302854cc19d5498
Gilles Baatz <gilles.baatz@cismet.de>
2014-03-28 14:50:31 +0100

    #158 removed unnecessary parameters from TifferDownload and added two helper methods which retrieve the URL for lowRes pictures and the format of highres pictures.

--- src/main/java/de/cismet/cids/custom/utils/TifferDownload.java
+++ src/main/java/de/cismet/cids/custom/utils/TifferDownload.java
@@ -15,15 +15,22 @@ import Sirius.navigator.connection.SessionManager;
 
 import Sirius.util.image.ImageAnnotator;
 
+import org.apache.log4j.Logger;
+
 import java.awt.image.RenderedImage;
 
 import java.io.ByteArrayInputStream;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import javax.imageio.ImageIO;
 
 import de.cismet.cids.custom.wunda_blau.search.actions.TifferAction;
 
 import de.cismet.cids.server.actions.ServerActionParameter;
+
+import de.cismet.security.WebAccessManager;
 
 import de.cismet.tools.gui.downloadmanager.AbstractDownload;
 
@@ -39,47 +46,45 @@ import static de.cismet.cids.custom.wunda_blau.search.actions.TifferAction.Param
  */
 public class TifferDownload extends AbstractDownload {
 
+    //~ Static fields/initializers ---------------------------------------------
+
+    private static final Logger LOG = Logger.getLogger(TifferDownload.class);
+
+    private static final String[] IMAGE_FILE_FORMATS = { "jpg", "tiff" };
+
     //~ Instance fields --------------------------------------------------------
 
-    private String imageNumber;
-    private String location;
-    private String dateRecorded;
-    private String scale;
-    private String format;
+    private final String imageNumber;
+    private final String scale;
+    private final String format;
 
     //~ Constructors -----------------------------------------------------------
 
     /**
      * Creates a new TifferDownload object.
      *
-     * @param  directory     DOCUMENT ME!
-     * @param  title         DOCUMENT ME!
-     * @param  filename      DOCUMENT ME!
-     * @param  imageNumber   DOCUMENT ME!
-     * @param  location      DOCUMENT ME!
-     * @param  dateRecorded  DOCUMENT ME!
-     * @param  scale         DOCUMENT ME!
-     * @param  format        DOCUMENT ME!
+     * @param  directory    DOCUMENT ME!
+     * @param  title        DOCUMENT ME!
+     * @param  filename     DOCUMENT ME!
+     * @param  imageNumber  DOCUMENT ME!
+     * @param  scale        DOCUMENT ME!
      */
     public TifferDownload(final String directory,
             final String title,
             final String filename,
             final String imageNumber,
-            final String location,
-            final String dateRecorded,
-            final String scale,
-            final String format) {
+            final String scale) {
         this.imageNumber = imageNumber;
         this.directory = directory;
         this.title = title;
-        this.location = location;
-        this.dateRecorded = dateRecorded;
         this.scale = scale;
-        this.format = format;
 
         status = State.WAITING;
 
-        determineDestinationFile(filename, "." + format.toLowerCase());
+        format = getFormatOfHighResPicture(imageNumber);
+        if (format != null) {
+            determineDestinationFile(filename, "." + format.toLowerCase());
+        }
     }
 
     //~ Methods ----------------------------------------------------------------
@@ -90,6 +95,13 @@ public class TifferDownload extends AbstractDownload {
             return;
         }
 
+        if (format == null) {
+            log.error(
+                "No format found for the image. The image might not exist");
+            error(new Exception("No format found for the image. The image might not exist"));
+            return;
+        }
+
         status = State.RUNNING;
 
         stateChanged();
@@ -97,12 +109,6 @@ public class TifferDownload extends AbstractDownload {
         final ServerActionParameter paramNummer = new ServerActionParameter(
                 BILDNUMMER.toString(),
                 imageNumber);
-        final ServerActionParameter paramLocation = new ServerActionParameter(
-                ORT.toString(),
-                location);
-        final ServerActionParameter paramDateRecorded = new ServerActionParameter(
-                AUFNAHME_DATUM.toString(),
-                dateRecorded);
         final ServerActionParameter paramScale = new ServerActionParameter(
                 SCALE.toString(),
                 scale);
@@ -119,8 +125,6 @@ public class TifferDownload extends AbstractDownload {
                                 "WUNDA_BLAU",
                                 null,
                                 paramNummer,
-                                paramLocation,
-                                paramDateRecorded,
                                 paramScale,
                                 paramFormat,
                                 paramSubdir);
@@ -157,10 +161,60 @@ public class TifferDownload extends AbstractDownload {
      */
     private ServerActionParameter createSubDirForURL(final String imageNumber) {
         final char firstCharacter = imageNumber.charAt(0);
-        final String subdir = "SB/" + firstCharacter + "/";
+        final String subdir = "SB/" + firstCharacter + "/" + "SB_";
 
         return new ServerActionParameter(
                 SUBDIR.toString(),
                 subdir);
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @param   imageNumber  DOCUMENT ME!
+     *
+     * @return  DOCUMENT ME!
+     */
+    public static String getFormatOfHighResPicture(final String imageNumber) {
+        final char firstCharacter = imageNumber.charAt(0);
+        final String locationOfPreviewImage = "SB/" + firstCharacter + "/SB_" + imageNumber;
+        for (final String fileEnding : IMAGE_FILE_FORMATS) {
+            try {
+                final String urlName = "http://s102x003/archivar/" + locationOfPreviewImage + "." + fileEnding;
+                final URL url = new URL(urlName);
+                final boolean accessible = WebAccessManager.getInstance().checkIfURLaccessible(url);
+                if (accessible) {
+                    return fileEnding;
+                }
+            } catch (MalformedURLException ex) {
+                LOG.warn(ex, ex);
+            }
+        }
+        return null;
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @param   imageNumber  DOCUMENT ME!
+     *
+     * @return  DOCUMENT ME!
+     */
+    public static URL getURLOfLowResPicture(final String imageNumber) {
+        final char firstCharacter = imageNumber.charAt(0);
+        final String locationOfPreviewImage = "VB/" + firstCharacter + "/VB_" + imageNumber;
+        for (final String fileEnding : IMAGE_FILE_FORMATS) {
+            try {
+                final String urlName = "http://s102x003/archivar/" + locationOfPreviewImage + "." + fileEnding;
+                final URL url = new URL(urlName);
+                final boolean accessible = WebAccessManager.getInstance().checkIfURLaccessible(url);
+                if (accessible) {
+                    return url;
+                }
+            } catch (MalformedURLException ex) {
+                LOG.warn(ex, ex);
+            }
+        }
+        return null;
     }
 }
