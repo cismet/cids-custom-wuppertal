4a9caad73ec19d66481d5c73b45cc2c151db76b8
Gilles Baatz <gilles.baatz@cismet.de>
2014-06-18 14:47:16 +0200

    #234 removed annotations from Wbf_miet_typToStringConverter

--- src/main/java/de/cismet/cids/custom/tostringconverter/wunda_blau/Wbf_miet_typToStringConverter.java
+++ src/main/java/de/cismet/cids/custom/tostringconverter/wunda_blau/Wbf_miet_typToStringConverter.java
@@ -5,45 +5,26 @@
 *              ... and it just works.
 *
 ****************************************************/
-/*
- * ListToStringConverter.java
- *
- * Created on 6. Mai 2008, 10:59
- *
- * To change this template, choose Tools | Template Manager
- * and open the template in the editor.
- */
 package de.cismet.cids.custom.tostringconverter.wunda_blau;
-
-import de.cismet.cids.annotations.CidsAttribute;
 
 import de.cismet.cids.tools.CustomToStringConverter;
 
 /**
- * de.cismet.cids.toStringConverter.ListToStringConverter.
+ * DOCUMENT ME!
  *
- * @author   verkennis
  * @version  $Revision$, $Date$
  */
 public class Wbf_miet_typToStringConverter extends CustomToStringConverter {
-
-    //~ Instance fields --------------------------------------------------------
-
-    @CidsAttribute("Value")
-    public String value = null;
-
-    //~ Constructors -----------------------------------------------------------
-
-    /**
-     * Creates a new instance of ListToStringConverter.
-     */
-    public Wbf_miet_typToStringConverter() {
-    }
 
     //~ Methods ----------------------------------------------------------------
 
     @Override
     public String createString() {
-        return value;
+        final String value = (String)cidsBean.getProperty("Value");
+        if (value != null) {
+            return value;
+        } else {
+            return "";
+        }
     }
 }
