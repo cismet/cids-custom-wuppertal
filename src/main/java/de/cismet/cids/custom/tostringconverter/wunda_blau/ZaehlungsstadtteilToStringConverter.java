d54326b69663d830aafd665680e3fe0f5762f1d0
Gilles Baatz <gilles.baatz@cismet.de>
2014-06-24 15:48:58 +0200

    #234 removed cids annotations from all ToStringConverter in Wunda

--- src/main/java/de/cismet/cids/custom/tostringconverter/wunda_blau/ZaehlungsstadtteilToStringConverter.java
+++ src/main/java/de/cismet/cids/custom/tostringconverter/wunda_blau/ZaehlungsstadtteilToStringConverter.java
@@ -5,37 +5,23 @@
 *              ... and it just works.
 *
 ****************************************************/
-/*
- * CategoryToStringConverter.java
- *
- * Created on 6. August 2007, 11:22
- *
- * To change this template, choose Tools | Template Manager
- * and open the template in the editor.
- */
 package de.cismet.cids.custom.tostringconverter.wunda_blau;
-
-import de.cismet.cids.annotations.CidsAttribute;
 
 import de.cismet.cids.tools.CustomToStringConverter;
 
 /**
- * de.cismet.cids.toStringConverter.ZaehlungsstadtteilToStringConverter.
+ * DOCUMENT ME!
  *
  * @author   verkennis
  * @version  $Revision$, $Date$
  */
 public class ZaehlungsstadtteilToStringConverter extends CustomToStringConverter {
 
-    //~ Instance fields --------------------------------------------------------
-
-    @CidsAttribute("Stadtbezirk")
-    public String stadtbez = null;
-
     //~ Methods ----------------------------------------------------------------
 
     @Override
     public String createString() {
-        return stadtbez;
+        final String stadtteil = (String)cidsBean.getProperty("stadtteil");
+        return stadtteil;
     }
 }
