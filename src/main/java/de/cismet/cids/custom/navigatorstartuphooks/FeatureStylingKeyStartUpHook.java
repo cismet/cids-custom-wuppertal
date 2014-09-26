8fa7a3eb06b724409e124d4fee57138a3e003f1a
helllth <th@cismet.de>
2014-09-26 11:32:09 +0200

    minor jalopy changes, removed unused imports

--- src/main/java/de/cismet/cids/custom/navigatorstartuphooks/FeatureStylingKeyStartUpHook.java
+++ src/main/java/de/cismet/cids/custom/navigatorstartuphooks/FeatureStylingKeyStartUpHook.java
@@ -12,27 +12,14 @@
 package de.cismet.cids.custom.navigatorstartuphooks;
 
 import Sirius.navigator.connection.SessionManager;
-import Sirius.navigator.exception.ConnectionException;
 
 import org.apache.log4j.Logger;
 
 import org.openide.util.lookup.ServiceProvider;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-
-import de.cismet.cids.custom.butler.*;
-import de.cismet.cids.custom.utils.butler.ButlerRequestInfo;
-import de.cismet.cids.custom.wunda_blau.search.actions.ButlerQueryAction;
-
-import de.cismet.cids.server.actions.ServerActionParameter;
-
 import de.cismet.cismap.commons.interaction.CismapBroker;
 
 import de.cismet.tools.configuration.StartupHook;
-
-import de.cismet.tools.gui.downloadmanager.DownloadManager;
-import de.cismet.tools.gui.downloadmanager.MultipleDownload;
 
 /**
  * DOCUMENT ME!
