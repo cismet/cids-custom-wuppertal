3ec9432c0dfa500e02000a5a6f176620038ebf66
Jean-Michel Ruiz <jean.ruiz@cismet.de>
2020-09-10 08:50:59 +0200

    issue minor jalopy changes

--- src/main/java/de/cismet/cids/custom/objectrenderer/wunda_blau/FSReloadProduktDialog.java
+++ src/main/java/de/cismet/cids/custom/objectrenderer/wunda_blau/FSReloadProduktDialog.java
@@ -153,9 +153,9 @@ public class FSReloadProduktDialog extends javax.swing.JDialog implements Connec
      *
      * @param  evt  DOCUMENT ME!
      */
-    private void jButton1ActionPerformed(final java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void jButton1ActionPerformed(final java.awt.event.ActionEvent evt) { //GEN-FIRST:event_jButton1ActionPerformed
         dispose();
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }                                                                            //GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * DOCUMENT ME!
