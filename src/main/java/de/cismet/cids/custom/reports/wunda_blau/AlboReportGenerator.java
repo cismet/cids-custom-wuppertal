3c8f383547202148435313de8fa2b6a49ce4e7e6
Jean-Michel Ruiz <jean.ruiz@cismet.de>
2021-01-12 14:40:38 +0100

    issue #566 report action

--- src/main/java/de/cismet/cids/custom/reports/wunda_blau/AlboReportGenerator.java
+++ src/main/java/de/cismet/cids/custom/reports/wunda_blau/AlboReportGenerator.java
@@ -12,26 +12,14 @@
  */
 package de.cismet.cids.custom.reports.wunda_blau;
 
-import Sirius.navigator.connection.SessionManager;
-import Sirius.navigator.exception.ConnectionException;
-
-import Sirius.server.newuser.User;
-
-import net.sf.jasperreports.engine.JRDataSource;
-import net.sf.jasperreports.engine.JasperReport;
-import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import Sirius.server.middleware.types.MetaObjectNode;
 
 import java.awt.Component;
 
-import java.util.Arrays;
-
-import de.cismet.cids.custom.utils.WundaBlauServerResources;
+import de.cismet.cids.custom.utils.ByteArrayActionDownload;
+import de.cismet.cids.custom.wunda_blau.search.actions.AlboVorgangReportServerAction;
 
 import de.cismet.cids.dynamics.CidsBean;
-
-import de.cismet.cids.server.actions.GetServerResourceServerAction;
-
-import de.cismet.cismap.commons.gui.printing.JasperReportDownload;
 
 import de.cismet.connectioncontext.ConnectionContext;
 
@@ -48,49 +36,9 @@ public class AlboReportGenerator {
 
     //~ Static fields/initializers ---------------------------------------------
 
-    private static final WundaBlauServerResources FLAECHE_REPORT_SERVER_RESOURCE =
-        WundaBlauServerResources.ALBO_FLAECHE_JASPER;
-    private static final WundaBlauServerResources VORGANG_REPORT_SERVER_RESOURCE =
-        WundaBlauServerResources.ALBO_VORGANG_JASPER;
     private static final org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(AlboReportGenerator.class);
 
     //~ Methods ----------------------------------------------------------------
-
-    /**
-     * DOCUMENT ME!
-     *
-     * @param  flaecheBean        DOCUMENT ME!
-     * @param  parent             DOCUMENT ME!
-     * @param  connectionContext  DOCUMENT ME!
-     */
-    public static void startFlaecheReportDownload(final CidsBean flaecheBean,
-            final Component parent,
-            final ConnectionContext connectionContext) {
-        try {
-            final JasperReport jasperReport = getReport(FLAECHE_REPORT_SERVER_RESOURCE, connectionContext);
-
-            if (DownloadManagerDialog.getInstance().showAskingForUserTitleDialog(parent)) {
-                final String jobname = DownloadManagerDialog.getInstance().getJobName();
-
-                final JasperReportDownload.JasperReportParametersGenerator parametersGenerator =
-                    new AlboReportFlaecheParametersGenerator(flaecheBean, connectionContext);
-                final JasperReportDownload.JasperReportDataSourceGenerator dataSourceGenerator =
-                    new AlboReportFlaecheDataSourceGenerator(flaecheBean, connectionContext);
-
-                final String erhebungsnummer = (String)flaecheBean.getProperty("erhebungsnummer");
-                DownloadManager.instance()
-                        .add(new JasperReportDownload(
-                                jasperReport,
-                                parametersGenerator,
-                                dataSourceGenerator,
-                                jobname,
-                                String.format("Altlastenkataster - Fläche %s", erhebungsnummer),
-                                String.format("albo_flaeche_%s", erhebungsnummer)));
-            }
-        } catch (final Exception ex) {
-            LOG.fatal(ex, ex);
-        }
-    }
 
     /**
      * DOCUMENT ME!
@@ -103,51 +51,23 @@ public class AlboReportGenerator {
             final Component parent,
             final ConnectionContext connectionContext) {
         try {
-            final JasperReport jasperReport = getReport(VORGANG_REPORT_SERVER_RESOURCE, connectionContext);
-
             if (DownloadManagerDialog.getInstance().showAskingForUserTitleDialog(parent)) {
                 final String jobname = DownloadManagerDialog.getInstance().getJobName();
-
-                final JasperReportDownload.JasperReportParametersGenerator parametersGenerator =
-                    new AlboReportVorgangParametersGenerator(vorgangBean, connectionContext);
-                final JasperReportDownload.JasperReportDataSourceGenerator dataSourceGenerator =
-                    new AlboReportVorgangDataSourceGenerator(vorgangBean, connectionContext);
-
                 final String vorgang = (String)vorgangBean.getProperty("schluessel");
+
                 DownloadManager.instance()
-                        .add(new JasperReportDownload(
-                                jasperReport,
-                                parametersGenerator,
-                                dataSourceGenerator,
-                                jobname,
+                        .add(new ByteArrayActionDownload(
+                                AlboVorgangReportServerAction.TASK_NAME,
+                                new MetaObjectNode(vorgangBean),
+                                null,
                                 String.format("Altlastenkataster - Vorgang %s", vorgang),
-                                String.format("albo_vorgang_%s", vorgang)));
+                                jobname,
+                                String.format("albo_vorgang_%s", vorgang),
+                                ".pdf",
+                                connectionContext));
             }
         } catch (final Exception ex) {
             LOG.fatal(ex, ex);
         }
-    }
-
-    /**
-     * DOCUMENT ME!
-     *
-     * @param   reportResource     DOCUMENT ME!
-     * @param   connectionContext  DOCUMENT ME!
-     *
-     * @return  DOCUMENT ME!
-     *
-     * @throws  ConnectionException  DOCUMENT ME!
-     */
-    private static JasperReport getReport(final WundaBlauServerResources reportResource,
-            final ConnectionContext connectionContext) throws ConnectionException {
-        final User user = SessionManager.getSession().getUser();
-        final JasperReport jasperReport = (JasperReport)SessionManager.getProxy()
-                    .executeTask(
-                            user,
-                            GetServerResourceServerAction.TASK_NAME,
-                            "WUNDA_BLAU",
-                            reportResource.getValue(),
-                            connectionContext);
-        return jasperReport;
     }
 }
