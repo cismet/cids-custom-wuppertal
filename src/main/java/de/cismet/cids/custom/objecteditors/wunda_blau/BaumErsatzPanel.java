4caaa1bab7769a7cbe2af4f69b3b11cfba6e06dc
therter <thorsten.herter@cismet.de>
2021-11-17 17:11:08 +0100

    #608 jalopy changes

--- src/main/java/de/cismet/cids/custom/objecteditors/wunda_blau/BaumErsatzPanel.java
+++ src/main/java/de/cismet/cids/custom/objecteditors/wunda_blau/BaumErsatzPanel.java
@@ -1122,31 +1122,31 @@ public class BaumErsatzPanel extends javax.swing.JPanel implements Disposable,
      *
      * @param  evt  DOCUMENT ME!
      */
-    private void btnAddKontActionPerformed(final ActionEvent evt) {//GEN-FIRST:event_btnAddKontActionPerformed
+    private void btnAddKontActionPerformed(final ActionEvent evt) { //GEN-FIRST:event_btnAddKontActionPerformed
         if (getCidsBean() != null) {
             TableUtils.addObjectToTable(xtKont, TABLE_NAME__KONTROLLE, getConnectionContext());
             setChangeFlag();
         }
-    }//GEN-LAST:event_btnAddKontActionPerformed
+    }                                                               //GEN-LAST:event_btnAddKontActionPerformed
 
     /**
      * DOCUMENT ME!
      *
      * @param  evt  DOCUMENT ME!
      */
-    private void btnRemKontActionPerformed(final ActionEvent evt) {//GEN-FIRST:event_btnRemKontActionPerformed
+    private void btnRemKontActionPerformed(final ActionEvent evt) { //GEN-FIRST:event_btnRemKontActionPerformed
         if (getCidsBean() != null) {
             TableUtils.removeObjectsFromTable(xtKont);
             setChangeFlag();
         }
-    }//GEN-LAST:event_btnRemKontActionPerformed
+    }                                                               //GEN-LAST:event_btnRemKontActionPerformed
 
     /**
      * DOCUMENT ME!
      *
      * @param  evt  DOCUMENT ME!
      */
-    private void cbArtEActionPerformed(final ActionEvent evt) {//GEN-FIRST:event_cbArtEActionPerformed
+    private void cbArtEActionPerformed(final ActionEvent evt) { //GEN-FIRST:event_cbArtEActionPerformed
         if ((getCidsBean() != null) && (getCidsBean().getProperty(FIELD__ART) != null)) {
             cbSorte.setSelectedItem(null);
             if (isEditor()) {
@@ -1154,14 +1154,14 @@ public class BaumErsatzPanel extends javax.swing.JPanel implements Disposable,
             }
             refreshSorte();
         }
-    }//GEN-LAST:event_cbArtEActionPerformed
+    }                                                           //GEN-LAST:event_cbArtEActionPerformed
 
     /**
      * DOCUMENT ME!
      *
      * @param  evt  DOCUMENT ME!
      */
-    private void cbStrasseActionPerformed(final java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cbStrasseActionPerformed
+    private void cbStrasseActionPerformed(final java.awt.event.ActionEvent evt) { //GEN-FIRST:event_cbStrasseActionPerformed
         if ((getCidsBean() != null) && (getCidsBean().getProperty(FIELD__STRASSE) != null)) {
             cbHNr.setSelectedItem(null);
             if (isEditor()) {
@@ -1169,7 +1169,7 @@ public class BaumErsatzPanel extends javax.swing.JPanel implements Disposable,
             }
             refreshHnr();
         }
-    }//GEN-LAST:event_cbStrasseActionPerformed
+    }                                                                             //GEN-LAST:event_cbStrasseActionPerformed
     /**
      * DOCUMENT ME!
      *
@@ -1463,10 +1463,10 @@ public class BaumErsatzPanel extends javax.swing.JPanel implements Disposable,
 
         final boolean isSetArt = saveErsatzBean.getProperty(FIELD__ART) != null;
         final boolean isSetPlanzung = saveErsatzBean.getProperty(FIELD__DATUM_P) != null;
-        final boolean isSetAnzahl = saveErsatzBean.getProperty(FIELD__ANZAHL) != null
-                && (Integer)saveErsatzBean.getProperty(FIELD__ANZAHL) > 0;
-        
-        if (isSetArt || isSetAnzahl || isSetPlanzung){
+        final boolean isSetAnzahl = (saveErsatzBean.getProperty(FIELD__ANZAHL) != null)
+                    && ((Integer)saveErsatzBean.getProperty(FIELD__ANZAHL) > 0);
+
+        if (isSetArt || isSetAnzahl || isSetPlanzung) {
             // Straße muss angegeben werden
             try {
                 if (saveErsatzBean.getProperty(FIELD__STRASSE) == null) {
@@ -1497,7 +1497,7 @@ public class BaumErsatzPanel extends javax.swing.JPanel implements Disposable,
                 LOG.warn("Geom not given.", ex);
                 save = false;
             }
-        } else{
+        } else {
             // Bis muss angegeben werden
             try {
                 if (saveErsatzBean.getProperty(FIELD__BIS) == null) {
@@ -1514,10 +1514,10 @@ public class BaumErsatzPanel extends javax.swing.JPanel implements Disposable,
         // Anzahl Pflichtattribut, wenn gepflanzt oder Art angegeben
         try {
             if ((isSetArt || isSetPlanzung) && !(isSetAnzahl)) {
-                    LOG.warn("No count specified. Skip persisting.");
-                    errorMessage.append(NbBundle.getMessage(BaumErsatzPanel.class, BUNDLE_NOCOUNT));
-                    save = false;
-                }
+                LOG.warn("No count specified. Skip persisting.");
+                errorMessage.append(NbBundle.getMessage(BaumErsatzPanel.class, BUNDLE_NOCOUNT));
+                save = false;
+            }
         } catch (final MissingResourceException ex) {
             LOG.warn("Count not given.", ex);
             save = false;
@@ -1546,7 +1546,7 @@ public class BaumErsatzPanel extends javax.swing.JPanel implements Disposable,
             LOG.warn("Datum not given.", ex);
             save = false;
         }
-        
+
         // Kontrolle
         try {
             final Collection<CidsBean> controlCollection = saveErsatzBean.getBeanCollectionProperty(FIELD__KONTROLLE);
