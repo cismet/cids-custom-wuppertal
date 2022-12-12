c1fa21afb2af426ed480ecadb36189f0b4bda8cc
Jean-Michel Ruiz <jean.ruiz@cismet.de>
2022-12-12 16:03:32 +0100

    issue #643 minor label change

--- src/main/java/de/cismet/cids/custom/objectrenderer/wunda_blau/VermessungsunterlagenauftragRenderer.java
+++ src/main/java/de/cismet/cids/custom/objectrenderer/wunda_blau/VermessungsunterlagenauftragRenderer.java
@@ -1552,33 +1552,33 @@ public class VermessungsunterlagenauftragRenderer extends JPanel implements Cids
      *
      * @param  evt  DOCUMENT ME!
      */
-    private void jToggleButton1ActionPerformed(final java.awt.event.ActionEvent evt) { //GEN-FIRST:event_jToggleButton1ActionPerformed
+    private void jToggleButton1ActionPerformed(final java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jToggleButton1ActionPerformed
         mappingComponent.getFeatureCollection().removeFeature(geometrieFeature);
         mappingComponent.getFeatureCollection().removeFeature(geometrieSaumFeature);
         mappingComponent.getFeatureCollection().addFeature(flurstueckeFeature);
-    }                                                                                  //GEN-LAST:event_jToggleButton1ActionPerformed
+    }//GEN-LAST:event_jToggleButton1ActionPerformed
 
     /**
      * DOCUMENT ME!
      *
      * @param  evt  DOCUMENT ME!
      */
-    private void jToggleButton2ActionPerformed(final java.awt.event.ActionEvent evt) { //GEN-FIRST:event_jToggleButton2ActionPerformed
+    private void jToggleButton2ActionPerformed(final java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jToggleButton2ActionPerformed
         mappingComponent.getFeatureCollection().addFeature(geometrieFeature);
         mappingComponent.getFeatureCollection().removeFeature(geometrieSaumFeature);
         mappingComponent.getFeatureCollection().removeFeature(flurstueckeFeature);
-    }                                                                                  //GEN-LAST:event_jToggleButton2ActionPerformed
+    }//GEN-LAST:event_jToggleButton2ActionPerformed
 
     /**
      * DOCUMENT ME!
      *
      * @param  evt  DOCUMENT ME!
      */
-    private void jToggleButton3ActionPerformed(final java.awt.event.ActionEvent evt) { //GEN-FIRST:event_jToggleButton3ActionPerformed
+    private void jToggleButton3ActionPerformed(final java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jToggleButton3ActionPerformed
         mappingComponent.getFeatureCollection().removeFeature(geometrieFeature);
         mappingComponent.getFeatureCollection().addFeature(geometrieSaumFeature);
         mappingComponent.getFeatureCollection().removeFeature(flurstueckeFeature);
-    }                                                                                  //GEN-LAST:event_jToggleButton3ActionPerformed
+    }//GEN-LAST:event_jToggleButton3ActionPerformed
 
     /**
      * DOCUMENT ME!
@@ -1696,16 +1696,16 @@ public class VermessungsunterlagenauftragRenderer extends JPanel implements Cids
      *
      * @param  evt  DOCUMENT ME!
      */
-    private void jxhStatusAltActionPerformed(final java.awt.event.ActionEvent evt) { //GEN-FIRST:event_jxhStatusAltActionPerformed
+    private void jxhStatusAltActionPerformed(final java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jxhStatusAltActionPerformed
         downloadProduktOrShowError();
-    }                                                                                //GEN-LAST:event_jxhStatusAltActionPerformed
+    }//GEN-LAST:event_jxhStatusAltActionPerformed
 
     /**
      * DOCUMENT ME!
      *
      * @param  evt  DOCUMENT ME!
      */
-    private void jList2MouseClicked(final java.awt.event.MouseEvent evt) { //GEN-FIRST:event_jList2MouseClicked
+    private void jList2MouseClicked(final java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jList2MouseClicked
         if (evt.getClickCount() > 1) {
             final Object selObject = jList2.getSelectedValue();
             if (selObject instanceof String) {
@@ -1754,14 +1754,14 @@ public class VermessungsunterlagenauftragRenderer extends JPanel implements Cids
                 }
             }
         }
-    } //GEN-LAST:event_jList2MouseClicked
+    }//GEN-LAST:event_jList2MouseClicked
 
     /**
      * DOCUMENT ME!
      *
      * @param  evt  DOCUMENT ME!
      */
-    private void jCheckBox1ActionPerformed(final java.awt.event.ActionEvent evt) { //GEN-FIRST:event_jCheckBox1ActionPerformed
+    private void jCheckBox1ActionPerformed(final java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jCheckBox1ActionPerformed
         jCheckBox1.setEnabled(false);
         new SwingWorker<CidsBean, Object>() {
 
@@ -1791,14 +1791,14 @@ public class VermessungsunterlagenauftragRenderer extends JPanel implements Cids
                     }
                 }
             }.execute();
-    } //GEN-LAST:event_jCheckBox1ActionPerformed
+    }//GEN-LAST:event_jCheckBox1ActionPerformed
 
     /**
      * DOCUMENT ME!
      *
      * @param  evt  DOCUMENT ME!
      */
-    private void jList4MouseClicked(final java.awt.event.MouseEvent evt) { //GEN-FIRST:event_jList4MouseClicked
+    private void jList4MouseClicked(final java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jList4MouseClicked
         if (evt.getClickCount() > 1) {
             final int index = jList4.getSelectedIndex();
             final VermessungsunterlagenException ex = (VermessungsunterlagenException)
@@ -1814,27 +1814,27 @@ public class VermessungsunterlagenauftragRenderer extends JPanel implements Cids
                     null);
             JXErrorPane.showDialog(this, errorInfo);
         }
-    }                                                                      //GEN-LAST:event_jList4MouseClicked
+    }//GEN-LAST:event_jList4MouseClicked
 
     /**
      * DOCUMENT ME!
      *
      * @param  evt  DOCUMENT ME!
      */
-    private void jButton1ActionPerformed(final java.awt.event.ActionEvent evt) { //GEN-FIRST:event_jButton1ActionPerformed
+    private void jButton1ActionPerformed(final java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         exceptionDialog.setVisible(false);
         ((DefaultListModel<VermessungsunterlagenException>)jList4.getModel()).clear();
         jLabel9.setText("");
-    }                                                                            //GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * DOCUMENT ME!
      *
      * @param  evt  DOCUMENT ME!
      */
-    private void jxhStatusNeuValueActionPerformed(final java.awt.event.ActionEvent evt) { //GEN-FIRST:event_jxhStatusNeuValueActionPerformed
+    private void jxhStatusNeuValueActionPerformed(final java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jxhStatusNeuValueActionPerformed
         downloadProduktOrShowError();
-    }                                                                                     //GEN-LAST:event_jxhStatusNeuValueActionPerformed
+    }//GEN-LAST:event_jxhStatusNeuValueActionPerformed
 
     /**
      * DOCUMENT ME!
@@ -2187,7 +2187,7 @@ public class VermessungsunterlagenauftragRenderer extends JPanel implements Cids
                                         search,
                                         getConnectionContext());
                         if ((res == null) || res.isEmpty()) {
-                            return "nicht registrierte Vermessungsstelle";
+                            return "nicht in WuNDa registrierte Vermessungsstelle";
                         } else {
                             return ((VermessungsStellenSearchResult)res.iterator().next()).getName();
                         }
