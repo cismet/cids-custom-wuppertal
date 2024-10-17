2550bec3d7aebaa26554ce7b1ef69de42a04fd3a
sandra-simmert <sandra.simmert@stadt.wuppertal.de>
2024-10-17 11:06:01 +0200

    Winterruhe

---
+++ src/main/java/de/cismet/cids/custom/objecteditors/wunda_blau/TwWinterruheEditor.java
@@ -0,0 +1,379 @@
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.cismet.cids.custom.objecteditors.wunda_blau;
+
+import Sirius.navigator.ui.RequestsFullSizeComponent;
+
+
+import org.apache.log4j.Logger;
+
+import org.jdesktop.beansbinding.AutoBinding;
+import org.jdesktop.beansbinding.BeanProperty;
+import org.jdesktop.beansbinding.Binding;
+import org.jdesktop.beansbinding.BindingGroup;
+import org.jdesktop.beansbinding.Bindings;
+import org.jdesktop.beansbinding.ELProperty;
+
+import org.openide.util.NbBundle;
+
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+
+import javax.swing.*;
+
+import de.cismet.cids.custom.objecteditors.utils.RendererTools;
+
+import de.cismet.cids.dynamics.CidsBean;
+import de.cismet.cids.editors.DefaultBindableDateChooser;
+
+import de.cismet.cids.editors.DefaultCustomObjectEditor;
+import de.cismet.cids.editors.SaveVetoable;
+
+import de.cismet.cids.tools.metaobjectrenderer.CidsBeanRenderer;
+
+import de.cismet.connectioncontext.ConnectionContext;
+
+import de.cismet.tools.gui.RoundedPanel;
+import de.cismet.tools.gui.StaticSwingTools;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.MissingResourceException;
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author   sandra
+ * @version  $Revision$, $Date$
+ */
+public class TwWinterruheEditor extends DefaultCustomObjectEditor implements CidsBeanRenderer,
+    SaveVetoable,
+    RequestsFullSizeComponent {
+
+    //~ Static fields/initializers ---------------------------------------------
+
+    private static final Logger LOG = Logger.getLogger(TwWinterruheEditor.class);
+
+    public static final String FIELD__START = "datum_start";
+    public static final String FIELD__ENDE = "datum_ende";
+    public static final String FIELD__ID = "id";
+    public static final String TABLE_NAME = "tw_winterruhe";
+
+    public static final String BUNDLE_NOSTART = "TwWinterruheEditor.isOkForSaving().noStart";
+    public static final String BUNDLE_NOENDE = "TwWinterruheEditor.isOkForSaving().noEnde";
+    public static final String BUNDLE_NOBEFORE = "TwWinterruheEditor.isOkForSaving().noBefore";
+    public static final String BUNDLE_PANE_PREFIX = "TwWinterruheEditor.isOkForSaving().JOptionPane.message.prefix";
+    public static final String BUNDLE_PANE_SUFFIX = "TwWinterruheEditor.isOkForSaving().JOptionPane.message.suffix";
+    public static final String BUNDLE_PANE_TITLE = "TwWinterruheEditor.isOkForSaving().JOptionPane.title";
+
+    public static final String TITLE = "Winterzeit";
+    //~ Instance fields --------------------------------------------------------
+
+
+    private final boolean editor;
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    DefaultBindableDateChooser dcEnde;
+    DefaultBindableDateChooser dcStart;
+    private JLabel lblEnde;
+    private JLabel lblStart;
+    private JPanel panContent;
+    private JPanel panFillerUnten;
+    private JPanel panFillerUnten1;
+    private JPanel panName;
+    private BindingGroup bindingGroup;
+    // End of variables declaration//GEN-END:variables
+
+    //~ Constructors -----------------------------------------------------------
+
+    /**
+     * Creates new form.
+     */
+    public TwWinterruheEditor() {
+        this(true);
+    }
+
+    /**
+     * Creates a new TwMassnahmenEditor object.
+     *
+     * @param  boolEditor  DOCUMENT ME!
+     */
+    public TwWinterruheEditor(final boolean boolEditor) {
+        this.editor = boolEditor;
+    }
+
+    //~ Methods ----------------------------------------------------------------
+
+    @Override
+    public boolean isOkForSaving() {
+        boolean save = true;
+        final StringBuilder errorMessage = new StringBuilder();
+
+            
+        // dateTime Beginn vorhanden
+        try {
+            if (getCidsBean().getProperty(FIELD__START) == null) {
+                LOG.warn("No beginn specified. Skip persisting.");
+                errorMessage.append(NbBundle.getMessage(TwWinterruheEditor.class, BUNDLE_NOSTART));
+                save = false;
+            } else {
+                // dateTime Ende vorhanden
+                try {
+                    if (getCidsBean().getProperty(FIELD__ENDE) == null) {
+                        LOG.warn("No ende specified. Skip persisting.");
+                        errorMessage.append(NbBundle.getMessage(TwWinterruheEditor.class, BUNDLE_NOENDE));
+                        save = false;
+                    } else {
+                        final LocalDate ldStart;
+                        ldStart = dcStart.getDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+                        final LocalDate ldEnde;
+                        ldEnde = dcStart.getDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+                        if (!ldStart.isBefore(ldEnde)) {
+                            LOG.warn("Start not before Ende. Skip persisting.");
+                            errorMessage.append(NbBundle.getMessage(TwWinterruheEditor.class, BUNDLE_NOBEFORE));
+                            save = false;
+                        }
+                    }    
+                } catch (final MissingResourceException ex) {
+                    LOG.warn("End not given.", ex);
+                    save = false;
+                }
+            }
+        } catch (final MissingResourceException ex) {
+            LOG.warn("Beginn not given.", ex);
+            save = false;
+        }
+
+        
+
+        if (errorMessage.length() > 0) {
+            JOptionPane.showMessageDialog(StaticSwingTools.getParentFrame(this),
+                NbBundle.getMessage(TwWinterruheEditor.class,
+                    BUNDLE_PANE_PREFIX)
+                        + errorMessage.toString()
+                        + NbBundle.getMessage(TwWinterruheEditor.class,
+                            BUNDLE_PANE_SUFFIX),
+                NbBundle.getMessage(TwWinterruheEditor.class,
+                    BUNDLE_PANE_TITLE),
+                JOptionPane.WARNING_MESSAGE);
+        }
+        return save;
+    }
+
+    @Override
+    public void initWithConnectionContext(final ConnectionContext connectionContext) {
+        super.initWithConnectionContext(connectionContext);
+        initComponents();
+
+        setReadOnly();
+    }
+
+    /**
+     * This method is called from within the constructor to initialize the form. WARNING: Do NOT modify this code. The
+     * content of this method is always regenerated by the Form Editor.
+     */
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+        GridBagConstraints gridBagConstraints;
+        bindingGroup = new BindingGroup();
+
+        panFillerUnten = new JPanel();
+        panContent = new RoundedPanel();
+        panName = new JPanel();
+        lblStart = new JLabel();
+        dcStart = new DefaultBindableDateChooser();
+        lblEnde = new JLabel();
+        dcEnde = new DefaultBindableDateChooser();
+        panFillerUnten1 = new JPanel();
+
+        setAutoscrolls(true);
+        setMinimumSize(new Dimension(600, 646));
+        setPreferredSize(new Dimension(600, 737));
+        setLayout(new GridBagLayout());
+
+        panFillerUnten.setName(""); // NOI18N
+        panFillerUnten.setOpaque(false);
+
+        GroupLayout panFillerUntenLayout = new GroupLayout(panFillerUnten);
+        panFillerUnten.setLayout(panFillerUntenLayout);
+        panFillerUntenLayout.setHorizontalGroup(panFillerUntenLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
+        );
+        panFillerUntenLayout.setVerticalGroup(panFillerUntenLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
+        );
+
+        gridBagConstraints = new GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 1;
+        gridBagConstraints.gridwidth = 2;
+        gridBagConstraints.fill = GridBagConstraints.BOTH;
+        gridBagConstraints.ipadx = 1;
+        gridBagConstraints.anchor = GridBagConstraints.WEST;
+        add(panFillerUnten, gridBagConstraints);
+
+        panContent.setAutoscrolls(true);
+        panContent.setMaximumSize(new Dimension(450, 2147483647));
+        panContent.setMinimumSize(new Dimension(450, 488));
+        panContent.setName(""); // NOI18N
+        panContent.setOpaque(false);
+        panContent.setPreferredSize(new Dimension(450, 961));
+        panContent.setLayout(new GridBagLayout());
+
+        panName.setOpaque(false);
+        panName.setLayout(new GridBagLayout());
+
+        lblStart.setFont(new Font("Tahoma", 1, 11)); // NOI18N
+        lblStart.setText("Start:");
+        gridBagConstraints = new GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.fill = GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.ipady = 10;
+        gridBagConstraints.anchor = GridBagConstraints.WEST;
+        gridBagConstraints.insets = new Insets(2, 0, 2, 5);
+        panName.add(lblStart, gridBagConstraints);
+
+        Binding binding = Bindings.createAutoBinding(AutoBinding.UpdateStrategy.READ_WRITE, this, ELProperty.create("${cidsBean.datum_start}"), dcStart, BeanProperty.create("date"));
+        binding.setSourceNullValue(null);
+        binding.setSourceUnreadableValue(null);
+        binding.setConverter(dcStart.getConverter());
+        bindingGroup.addBinding(binding);
+
+        gridBagConstraints = new GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.fill = GridBagConstraints.BOTH;
+        gridBagConstraints.anchor = GridBagConstraints.WEST;
+        gridBagConstraints.weightx = 1.0;
+        gridBagConstraints.insets = new Insets(2, 2, 2, 2);
+        panName.add(dcStart, gridBagConstraints);
+
+        lblEnde.setFont(new Font("Tahoma", 1, 11)); // NOI18N
+        lblEnde.setText("Ende:");
+        gridBagConstraints = new GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 1;
+        gridBagConstraints.fill = GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.ipady = 10;
+        gridBagConstraints.anchor = GridBagConstraints.WEST;
+        gridBagConstraints.insets = new Insets(2, 0, 2, 5);
+        panName.add(lblEnde, gridBagConstraints);
+
+        binding = Bindings.createAutoBinding(AutoBinding.UpdateStrategy.READ_WRITE, this, ELProperty.create("${cidsBean.datum_ende}"), dcEnde, BeanProperty.create("date"));
+        binding.setConverter(dcEnde.getConverter());
+        bindingGroup.addBinding(binding);
+
+        gridBagConstraints = new GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 1;
+        gridBagConstraints.fill = GridBagConstraints.BOTH;
+        gridBagConstraints.anchor = GridBagConstraints.WEST;
+        gridBagConstraints.insets = new Insets(2, 2, 2, 2);
+        panName.add(dcEnde, gridBagConstraints);
+
+        gridBagConstraints = new GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.fill = GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.weightx = 1.0;
+        gridBagConstraints.insets = new Insets(5, 5, 0, 5);
+        panContent.add(panName, gridBagConstraints);
+
+        panFillerUnten1.setName(""); // NOI18N
+        panFillerUnten1.setOpaque(false);
+
+        GroupLayout panFillerUnten1Layout = new GroupLayout(panFillerUnten1);
+        panFillerUnten1.setLayout(panFillerUnten1Layout);
+        panFillerUnten1Layout.setHorizontalGroup(panFillerUnten1Layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
+        );
+        panFillerUnten1Layout.setVerticalGroup(panFillerUnten1Layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
+        );
+
+        gridBagConstraints = new GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 1;
+        gridBagConstraints.fill = GridBagConstraints.BOTH;
+        gridBagConstraints.ipadx = 1;
+        gridBagConstraints.anchor = GridBagConstraints.WEST;
+        gridBagConstraints.weighty = 1.0;
+        panContent.add(panFillerUnten1, gridBagConstraints);
+
+        gridBagConstraints = new GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.fill = GridBagConstraints.BOTH;
+        gridBagConstraints.anchor = GridBagConstraints.NORTHWEST;
+        gridBagConstraints.weightx = 1.0;
+        gridBagConstraints.weighty = 1.0;
+        gridBagConstraints.insets = new Insets(10, 10, 10, 10);
+        add(panContent, gridBagConstraints);
+
+        bindingGroup.bind();
+    }// </editor-fold>//GEN-END:initComponents
+
+    @Override
+    public CidsBean getCidsBean() {
+        return cidsBean;
+    }
+
+    @Override
+    public void setCidsBean(final CidsBean cb) {
+        try {
+            bindingGroup.unbind();
+            this.cidsBean = cb;
+            if (cb != null) {
+                DefaultCustomObjectEditor.setMetaClassInformationToMetaClassStoreComponentsInBindingGroup(
+                    bindingGroup,
+                    cb,
+                    getConnectionContext());
+            }
+            bindingGroup.bind();
+        } catch (final Exception ex) {
+            LOG.warn("Error setCidsBean.", ex);
+        }
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @return  DOCUMENT ME!
+     */
+    private boolean isEditor() {
+        return this.editor;
+    }
+
+    /**
+     * DOCUMENT ME!
+     */
+    private void setReadOnly() {
+        if (!(isEditor())) {
+            RendererTools.makeReadOnly(dcStart);
+            RendererTools.makeReadOnly(dcEnde);
+        }
+    }
+
+    @Override
+    public String getTitle() {
+            return TITLE;
+    }
+
+    @Override
+    public void setTitle(final String string) {
+    }
+}
